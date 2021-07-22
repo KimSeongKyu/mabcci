e915e406b9415aef72d8f526e202225241bab2f6
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-22 14:55:49 +0900

    feat(kwj1270):#S05P12C107-66 백엔드 프로젝트 구성

---
+++ backend/mabcci/src/main/java/com/mabcci/MabcciApplication.java
@@ -0,0 +1,13 @@
+package com.mabcci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabcciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabcciApplication.class, args);
+    }
+
+}
