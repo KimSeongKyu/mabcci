d285131fdce8ab6e25e608ed0d52c6bc410c2a3b
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-23 14:10:43 +0900

    [S05P12C107-68] [BE-kwj1270] test: MemberRole 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/member/domain/MemberRoleTest.java
@@ -0,0 +1,23 @@
+package com.mabcci.member.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRoleTest {
+
+    @DisplayName("MemberRole 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(MemberRole.USER).isExactlyInstanceOf(MemberRole.class),
+                () -> assertThat(MemberRole.MABCCI).isExactlyInstanceOf(MemberRole.class),
+                () -> assertThat(MemberRole.ADMIN).isExactlyInstanceOf(MemberRole.class)
+        );
+    }
+
+
+
+}
