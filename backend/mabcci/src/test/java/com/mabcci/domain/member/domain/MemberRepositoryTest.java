daa892aad7bae09f16b28421219812447585097b
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-23 15:50:39 +0900

    [S05P12C107-68] [BE-kwj1270] test: MemberRepository save 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/member/domain/MemberRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/member/domain/MemberRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.mabcci.domain.member.domain;
 
+import com.mysql.cj.util.TestUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,16 +15,47 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class MemberRepositoryTest {
 
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        member = Member.builder()
+                .email("sample@email.com")
+                .password("validPassword")
+                .nickname("sample")
+                .phone("010-1234-5678")
+                .gender(Gender.MALE)
+                .role(MemberRole.USER)
+                .build();
+    }
+
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
 
     @DisplayName("MemberRepository 구현체 존재 여부 테스트")
     @Test
     void initialize() {
+        // when and then
         assertAll(
                 () -> assertThat(memberRepository).isNotNull(),
                 () -> assertThat(memberRepository).isInstanceOf(MemberRepository.class)
         );
+    }
+
+    @DisplayName("MemberRepository save 기능 테스트")
+    @Test
+    void save_test() {
+        // given
+        ReflectionTestUtils.setField(member, "id", 1);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        assertThat(savedMember.id()).isEqualTo(member.id());
     }
 
 }
