896991397b063ce86d6f26f610546372b8c88a6f
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-23 14:30:02 +0900

    [S05P12C107-68] [BE-kwj1270] test: Member 인스턴스의 password 일치 여부 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/member/domain/MemberTest.java
+++ backend/mabcci/src/test/java/com/mabcci/member/domain/MemberTest.java
@@ -1,30 +1,52 @@
 package com.mabcci.member.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberTest {
 
-    @DisplayName("Member 인스턴스 생성 여부 테스트")
-    @Test
-    void initialize() {
-        Member member = Member.builder()
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        member = Member.builder()
                 .email("sample@email.com")
-                .password("samplePassword")
+                .password("validPassword")
                 .nickname("sample")
                 .phone("010-1234-5678")
                 .gender(Gender.MALE)
                 .role(MemberRole.USER)
                 .build();
+    }
 
+    @DisplayName("Member 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        // when and then
         assertAll(
-                () -> Assertions.assertThat(member).isNotNull(),
-                () -> Assertions.assertThat(member).isExactlyInstanceOf(Member.class)
+                () -> assertThat(member).isNotNull(),
+                () -> assertThat(member).isExactlyInstanceOf(Member.class)
         );
 
+    }
+
+    @DisplayName("Member 인스턴스의 password 일치 여부 테스트")
+    @Test
+    void passwordCheck_test() {
+        // given
+        String validPassword = "validPassword";
+        String invalidPassword = "invalidPassword";
+
+        // when and then
+        assertAll(
+                () -> assertThat(member.checkPassoword(validPassword)).isTrue(),
+                () -> assertThat(member.checkPassoword(invalidPassword)).isFalse()
+        );
     }
 
 }
