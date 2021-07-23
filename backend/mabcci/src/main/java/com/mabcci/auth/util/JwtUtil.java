0cdca16510543ac70c2025e3f07610ea4e0ac24f
김성규 <kskyu610@gmail.com>
2021-07-23 17:04:54 +0900

    [S05P12C107-67] [BE-kskyu610] feat: JwtUtil 클래스 Header 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/auth/util/JwtUtil.java
+++ backend/mabcci/src/main/java/com/mabcci/auth/util/JwtUtil.java
@@ -2,6 +2,16 @@ package com.mabcci.auth.util;
 
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
 @NoArgsConstructor
 public class JwtUtil {
+
+    public Map<String, Object> createHeader() {
+        return Arrays.stream(Claim.values())
+                .collect(toMap(Claim::getKey, Claim::getValue));
+    }
 }
