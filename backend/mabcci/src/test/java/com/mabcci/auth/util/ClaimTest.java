804ea5d3847850d91644e9f27a2f05a00eb24a68
김성규 <kskyu610@gmail.com>
2021-07-23 16:12:38 +0900

    [S05P12C107-67] [BE-kskyu610] test: Claim 클래스 생성 테스트를 단일 테스트에서 반복 테스트로 리팩토링

--- backend/mabcci/src/test/java/com/mabcci/auth/util/ClaimTest.java
+++ backend/mabcci/src/test/java/com/mabcci/auth/util/ClaimTest.java
@@ -1,21 +1,27 @@
 package com.mabcci.auth.util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClaimTest {
 
-    @DisplayName(value = "생성 테스트")
-    @Test
-    public void constructTest() {
-        // given and when
-        Claim[] claims = Claim.values();
+    public static Stream<Arguments> provideClaimsForConstructTest() {
+        return Arrays.stream(Claim.values())
+                .map(claim -> Arguments.of(claim));
+    }
 
+    @DisplayName(value = "생성 테스트")
+    @ParameterizedTest(name = "{index}. ENUM TYPE : {0}")
+    @MethodSource(value = "provideClaimsForConstructTest")
+    public void constructTest(Claim claim) {
         // then
-        for(Claim claim : claims) {
-            assertThat(claim).isNotNull();
-        }
+        assertThat(claim).isNotNull();
     }
 }
