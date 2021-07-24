9b45f954ec5d416e136ac6b083d0832cab216dc2
김성규 <kskyu610@gmail.com>
2021-07-24 22:39:30 +0900

    [S05P12C107-67] [BE-kskyu610] feat: JwtUtil 클래스 Token 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/auth/util/JwtUtil.java
+++ backend/mabcci/src/main/java/com/mabcci/auth/util/JwtUtil.java
@@ -1,7 +1,12 @@
 package com.mabcci.auth.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.NoArgsConstructor;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Map;
@@ -10,6 +15,9 @@ import static java.util.stream.Collectors.toMap;
 
 @NoArgsConstructor
 public class JwtUtil {
+
+    private final static String SECRET_KEY =
+            "creative economy innovation services infrastructure fourth industrial revolution";
 
     public Map<String, Object> createHeader() {
         return Arrays.stream(Claim.values())
@@ -30,5 +38,15 @@ public class JwtUtil {
         payload.put(Claim.NICK_NAME, nickName);
 
         return payload;
+    }
+
+    public String createToken(final String nickName) {
+        Key secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .setHeader(createHeader())
+                .setClaims(createPayload(nickName))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
     }
 }
