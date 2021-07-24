1cf3be2edb4eada505804a1666dfe8c19fafbb08
김성규 <kskyu610@gmail.com>
2021-07-24 19:20:01 +0900

    [S05P12C107-67] [BE-kskyu610] feat: JwtUtil 클래스 payload 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/auth/util/JwtUtil.java
+++ backend/mabcci/src/main/java/com/mabcci/auth/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.mabcci.auth.util;
 import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
@@ -10,9 +11,26 @@ import static java.util.stream.Collectors.toMap;
 @NoArgsConstructor
 public class JwtUtil {
 
+    private final static long EXPIRATION_TIME = 1000 * 60 * 30;
+
     public Map<String, Object> createHeader() {
         return Arrays.stream(Claim.values())
                 .filter(claim -> claim.getType().equals(ClaimType.HEADER))
                 .collect(toMap(Claim::getKey, Claim::getValue));
+    }
+
+    public Map<String, Object> createPayload(final String nickName) {
+        final Map<String, Object> payload = Arrays.stream(Claim.values())
+                .filter(claim -> claim.getType().equals(ClaimType.PAYLOAD))
+                .collect(toMap(Claim::getKey, Claim::getValue));
+
+        final long currentTime = new Date().getTime();
+
+        payload.put("exp", currentTime + EXPIRATION_TIME);
+        payload.put("nbf", currentTime);
+        payload.put("iat", currentTime);
+        payload.put("nickName", nickName);
+
+        return payload;
     }
 }
