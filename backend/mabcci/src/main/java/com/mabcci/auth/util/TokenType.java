bbd4cf65897c39d42934874fddab1bf0ab1d791f
김성규 <kskyu610@gmail.com>
2021-07-24 23:59:48 +0900

    [S05P12C107-67] [BE-kskyu610] feat: TokenType 클래스 Expiration time 반환 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/auth/util/TokenType.java
+++ backend/mabcci/src/main/java/com/mabcci/auth/util/TokenType.java
@@ -1,12 +1,14 @@
 package com.mabcci.auth.util;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum TokenType {
 
     ACCESS_TOKEN(1000 * 60 * 30),
-    REFRESH_TOKEN(1000 * 60 * 60 * 24 * 30);
+    REFRESH_TOKEN(1000 * 60 * 60 * 24 * 7);
 
     private final long expirationTime;
 }
