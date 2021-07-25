fcc675b5efb369a6f679a4aa79b9596bdb72d77e
김성규 <kskyu610@gmail.com>
2021-07-25 17:13:37 +0900

    [S05P12C107-67] [BE-kskyu610] feat: LogoutRequestDto 클래스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/auth/dto/LogoutRequestDto.java
@@ -0,0 +1,9 @@
+package com.mabcci.auth.dto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LogoutRequestDto {
+
+    private final String email;
+}
--- backend/mabcci/src/test/java/com/mabcci/auth/controller/AuthControllerTest.java
+++ backend/mabcci/src/test/java/com/mabcci/auth/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.mabcci.auth.controller;
 
+import com.mabcci.auth.dto.LogoutRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
