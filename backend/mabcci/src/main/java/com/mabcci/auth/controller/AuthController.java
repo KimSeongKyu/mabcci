96ecc4f1d278d667b3ab8689ee3391a8374c82a1
김성규 <kskyu610@gmail.com>
2021-07-25 17:45:29 +0900

    [S05P12C107-67] [BE-kskyu610] feat: AuthController 클래스 로그아웃 API 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.mabcci.auth.controller;
+
+import com.mabcci.auth.dto.LogoutRequestDto;
+import com.mabcci.auth.exception.NotLoginMemberException;
+import com.mabcci.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/logout")
+    public ResponseEntity logout(@RequestBody final LogoutRequestDto logoutRequestDto) {
+        try {
+            authService.logout(logoutRequestDto.getEmail());
+        } catch (NotLoginMemberException notLoginMemberException) {
+            return ResponseEntity.badRequest().body(notLoginMemberException.toString());
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
