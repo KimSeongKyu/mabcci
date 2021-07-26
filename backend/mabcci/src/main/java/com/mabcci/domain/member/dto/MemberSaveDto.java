947ae5e3d3f005769c7ae6eabccc52fab1ce3f2d
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-26 09:46:11 +0900

    [S05P12C107-68] [BE-kwj1270] feat: MemberSaveDto 인스턴스 Member 엔티티로 변환 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/member/dto/MemberSaveDto.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/member/dto/MemberSaveDto.java
@@ -1,18 +1,27 @@
 package com.mabcci.domain.member.dto;
 
 import com.mabcci.domain.member.domain.Gender;
+import com.mabcci.domain.member.domain.Member;
+import com.mabcci.domain.member.domain.MemberRole;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 public class MemberSaveDto {
 
+    @Email
     private final String email;
+
+    @NotNull
     private final String password;
+
+    @NotNull
     private final String nickname;
+
+    @NotNull
     private final String phone;
+
+    @NotNull
     private final Gender gender;
 
     public MemberSaveDto(String email, String password, String nickname, String phone, Gender gender) {
@@ -41,5 +50,16 @@ public class MemberSaveDto {
 
     public Gender getGender() {
         return gender;
+    }
+
+    public Member entity() {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .phone(phone)
+                .gender(gender)
+                .role(MemberRole.USER)
+                .build();
     }
 }
