dff2e8f00244427fe9a00fe6ba98dc6d1fc4e2ea
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-27 17:28:45 +0900

    [S05P12C107-68] [BE-kwj1270] feat: Phone 인스턴스 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/model/Phone.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/model/Phone.java
@@ -1,4 +1,37 @@
 package com.mabcci.domain.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 public class Phone {
+
+    @Min(2)
+    @Max(3)
+    @NotEmpty
+    private String firstNumber;
+
+    @Min(3)
+    @Max(4)
+    @NotEmpty
+    private String secondNumber;
+
+    @Min(3)
+    @Max(4)
+    @NotEmpty
+    private String thirdNumber;
+
+    public static Phone from(final String firstNumber, final String secondNumber, final String thirdNumber) {
+        return new Phone(firstNumber, secondNumber, thirdNumber);
+    }
+
+    protected Phone() {
+    }
+
+    private Phone(final String firstNumber, final String secondNumber, final String thirdNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.thirdNumber = thirdNumber;
+    }
+
 }
