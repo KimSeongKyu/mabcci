3ca8e62b754c3f3dab152c24d4f691aa79e80a9c
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-27 14:01:32 +0900

    [S05P12C107-68] [BE-kwj1270] feat: Email 인스턴스 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/model/Email.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/model/Email.java
@@ -1,4 +1,25 @@
 package com.mabcci.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotEmpty;
+
+@Embeddable
 public class Email {
+
+    @javax.validation.constraints.Email
+    @NotEmpty
+    @Column(name = "email")
+    private String email;
+
+    public static Email of(final String value) {
+        return new Email(value);
+    }
+
+    protected Email() {
+    }
+
+    private Email(final String email) {
+        this.email = email;
+    }
 }
