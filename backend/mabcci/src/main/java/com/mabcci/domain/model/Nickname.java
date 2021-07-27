575c5979aff9a6d12bf799de28009fb6afbebf55
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-27 15:58:47 +0900

    [S05P12C107-68] [BE-kwj1270] feat: Nickname 인스턴스 equals() & hashCode() 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/model/Nickname.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/model/Nickname.java
@@ -3,6 +3,7 @@ package com.mabcci.domain.model;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
 @Embeddable
 public class Nickname {
@@ -23,5 +24,18 @@ public class Nickname {
 
     public String nickname() {
         return nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nickname nickname1 = (Nickname) o;
+        return Objects.equals(nickname(), nickname1.nickname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname());
     }
 }
