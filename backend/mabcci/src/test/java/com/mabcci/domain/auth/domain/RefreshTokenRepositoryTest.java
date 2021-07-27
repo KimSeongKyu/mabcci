6d19d3523308713eeca24f05913601821703b635
김성규 <kskyu610@gmail.com>
2021-07-27 13:52:28 +0900

    [S05P12C107-67] [BE-kskyu610] test: RefreshToken 클래스 Member 클래스와의 관계 수정 내용 적용

--- backend/mabcci/src/test/java/com/mabcci/domain/auth/domain/RefreshTokenRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/auth/domain/RefreshTokenRepositoryTest.java
@@ -1,10 +1,6 @@
 package com.mabcci.domain.auth.domain;
 
-import com.mabcci.domain.member.domain.Gender;
-import com.mabcci.domain.member.domain.Member;
 import com.mabcci.domain.member.domain.MemberRepository;
-import com.mabcci.domain.member.domain.MemberRole;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -30,29 +26,15 @@ public class RefreshTokenRepositoryTest {
     @Autowired
     private TestEntityManager testEntityManager;
 
-    private Member member;
-
-    @BeforeEach
-    public void setUp() {
-        member = Member.builder()
-                .email("sample@email.com")
-                .password("validPassword")
-                .nickname("sample")
-                .phone("010-1234-5678")
-                .gender(Gender.MALE)
-                .role(MemberRole.USER)
-                .build();
-
-        memberRepository.save(member);
-    }
 
     @DisplayName(value = "refresh token 유효성 검증 테스트")
     @ParameterizedTest(name = "{index}. refresh token: {0}")
     @NullAndEmptySource
     public void validateRefreshTokenTest(String value) {
         // given
+        String email = "example@example.com";
         RefreshToken refreshToken = RefreshToken.builder()
-                .member(member)
+                .email(email)
                 .refreshToken(value)
                 .build();
 
--- backend/mabcci/src/test/java/com/mabcci/domain/auth/service/AuthServiceTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/auth/service/AuthServiceTest.java
@@ -3,7 +3,6 @@ package com.mabcci.domain.auth.service;
 import com.mabcci.domain.auth.domain.RefreshToken;
 import com.mabcci.domain.auth.domain.RefreshTokenRepository;
 import com.mabcci.domain.auth.exception.NotLoginMemberException;
-import com.mabcci.domain.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,25 +25,22 @@ public class AuthServiceTest {
     @Mock
     private RefreshTokenRepository refreshTokenRepository;
 
-    @Mock
-    private Member member;
-
     @DisplayName(value = "로그아웃 성공 테스트")
     @Test
     public void logoutSuccessTest() {
         // given
         RefreshToken refreshToken = RefreshToken.builder()
-                .member(member)
+                .email("example@example.com")
                 .refreshToken("test.refresh.token")
                 .build();
-        doReturn(Optional.of(refreshToken)).when(refreshTokenRepository).findByMemberEmail(any());
+        doReturn(Optional.of(refreshToken)).when(refreshTokenRepository).findById(any());
         doNothing().when(refreshTokenRepository).delete(any());
 
         // when
         authService.logout(any());
 
         // then
-        verify(refreshTokenRepository, times(1)).findByMemberEmail(any());
+        verify(refreshTokenRepository, times(1)).findById(any());
         verify(refreshTokenRepository, times(1)).delete(any());
     }
 
