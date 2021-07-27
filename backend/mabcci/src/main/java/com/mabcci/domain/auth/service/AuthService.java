74c1146dad9e1e241163cb1bf12503d9c6205276
김성규 <kskyu610@gmail.com>
2021-07-27 15:33:25 +0900

    [S05P12C107-67] [BE-kskyu610] feat: AuthService 클래스 로그인 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/auth/service/AuthService.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/auth/service/AuthService.java
@@ -2,7 +2,14 @@ package com.mabcci.domain.auth.service;
 
 import com.mabcci.domain.auth.domain.RefreshToken;
 import com.mabcci.domain.auth.domain.RefreshTokenRepository;
+import com.mabcci.domain.auth.domain.TokenType;
+import com.mabcci.domain.auth.dto.LoginRequest;
+import com.mabcci.domain.auth.dto.LoginResponse;
 import com.mabcci.domain.auth.exception.NotLoginMemberException;
+import com.mabcci.domain.auth.util.JwtUtil;
+import com.mabcci.domain.member.domain.Member;
+import com.mabcci.domain.member.domain.MemberRepository;
+import com.mabcci.domain.member.exception.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public void logout(final String email) {
@@ -19,5 +28,22 @@ public class AuthService {
                 .orElseThrow(() -> new NotLoginMemberException(email));
 
         refreshTokenRepository.delete(refreshToken);
+    }
+
+    @Transactional
+    public LoginResponse login(final LoginRequest loginRequest) {
+        final String email = loginRequest.getEmail();
+        memberRepository.findByEmailAndPassword(email, loginRequest.getPassword())
+                .orElseThrow(MemberNotFoundException::new);
+
+        final String accessToken = jwtUtil.createToken(TokenType.ACCESS_TOKEN, email);
+        final String refreshToken = jwtUtil.createToken(TokenType.REFRESH_TOKEN, email);
+
+        refreshTokenRepository.save(RefreshToken.builder()
+                .email(email)
+                .refreshToken(refreshToken)
+                .build());
+
+        return new LoginResponse(accessToken, refreshToken);
     }
 }
