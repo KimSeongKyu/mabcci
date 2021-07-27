a470bccd84179c1e2ff82eae710f8006819d482f
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-27 15:56:48 +0900

    [S05P12C107-68] [BE-kwj1270] test: Nickname 인스턴스 equals() & hashCode() 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/model/NicknameTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/model/NicknameTest.java
@@ -40,5 +40,17 @@ class NicknameTest {
         assertThat(nickname.nickname()).isEqualTo(value);
     }
 
+    @DisplayName("Nickname 인스턴스 equals() & hashCode() 기능 테스트")
+    @Test
+    void equals_and_hashcode_test() {
+        final String value = "nickname";
+        final Nickname nickname = Nickname.of(value);
+        final Nickname other = Nickname.of(value);
+
+        assertAll(
+                () -> assertThat(nickname).isEqualTo(other),
+                () -> assertThat(nickname.hashCode()).isEqualTo(other.hashCode())
+        );
+    }
 
 }
