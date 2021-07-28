24d0f23797af4133e7ff44dd21eab8a8055c1299
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-28 14:16:22 +0900

    [S05P12C107-68] [BE-kwj1270] test: MemberService 인스턴스 delete() 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/member/application/MemberServiceTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/member/application/MemberServiceTest.java
@@ -1,7 +1,9 @@
 package com.mabcci.domain.member.application;
 
+import com.mabcci.domain.member.domain.Member;
 import com.mabcci.domain.member.domain.MemberRepository;
 import com.mabcci.domain.member.dto.MemberResponseDto;
+import com.mabcci.domain.model.Password;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static com.mabcci.domain.member.domain.MemberTest.MEMBER;
@@ -16,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
@@ -52,6 +58,34 @@ class MemberServiceTest {
                 () -> assertThat(memberResponseDto.getGender()).isEqualTo(MEMBER.gender()),
                 () -> assertThat(memberResponseDto.getRole()).isEqualTo(MEMBER.role())
         );
+    }
+
+    @DisplayName("MemberService 인스턴스 findAll() 기능 테스트")
+    @Test
+    void findAll_test() {
+        final List list = new ArrayList(Arrays.asList(MEMBER));
+        given(memberRepository.findAll()).willReturn((list));
+        final List<MemberResponseDto> memberResponseDtos = memberService.findAll();
+        final MemberResponseDto memberResponseDto = memberResponseDtos.get(0);
+
+        assertAll(
+                () -> assertThat(memberResponseDtos.size()).isEqualTo(1),
+                () -> assertThat(memberResponseDto.getEmail()).isEqualTo(MEMBER.email()),
+                () -> assertThat(memberResponseDto.getNickname()).isEqualTo(MEMBER.nickname()),
+                () -> assertThat(memberResponseDto.getGender()).isEqualTo(MEMBER.gender()),
+                () -> assertThat(memberResponseDto.getRole()).isEqualTo(MEMBER.role())
+        );
+    }
+
+    @DisplayName("MemberService 인스턴스 delete() 기능 테스트")
+    @Test
+    void delete_test() {
+        doReturn(Optional.ofNullable(MEMBER)).when(memberRepository).findByNicknameAndPassword(any(), any());
+        doNothing().when(memberRepository).delete(any());
+
+        memberService.delete(MEMBER.nickname(), Password.of("password"));
+
+        verify(memberRepository, times(1)).delete(any());
     }
 
 
