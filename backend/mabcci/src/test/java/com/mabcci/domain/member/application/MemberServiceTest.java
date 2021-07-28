a7a62de59a4f3d66ca75757cc37f6930ded142c3
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-28 14:05:33 +0900

    [S05P12C107-68] [BE-kwj1270] test: MemberService 인스턴스 findByNickname() 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/member/application/MemberServiceTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/member/application/MemberServiceTest.java
@@ -9,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static com.mabcci.domain.member.domain.MemberTest.MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,11 +26,25 @@ class MemberServiceTest {
     @InjectMocks
     private MemberService memberService;
 
-    @DisplayName("MemberService join() 기능 테스트")
+    @DisplayName("MemberService 인스턴스 join() 기능 테스트")
     @Test
     void join_test() {
         given(memberRepository.save(any())).willReturn(MEMBER);
         final MemberResponseDto memberResponseDto = memberService.join(MEMBER);
+
+        assertAll(
+                () -> assertThat(memberResponseDto.getEmail()).isEqualTo(MEMBER.email()),
+                () -> assertThat(memberResponseDto.getNickname()).isEqualTo(MEMBER.nickname()),
+                () -> assertThat(memberResponseDto.getGender()).isEqualTo(MEMBER.gender()),
+                () -> assertThat(memberResponseDto.getRole()).isEqualTo(MEMBER.role())
+        );
+    }
+
+    @DisplayName("MemberService 인스턴스 findByNickname() 기능 테스트")
+    @Test
+    void findByNickname_test() {
+        given(memberRepository.findByNickname(any())).willReturn(Optional.ofNullable(MEMBER));
+        final MemberResponseDto memberResponseDto = memberService.findByNickName(MEMBER.nickname());
 
         assertAll(
                 () -> assertThat(memberResponseDto.getEmail()).isEqualTo(MEMBER.email()),
