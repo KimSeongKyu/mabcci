67007de29a5662fb3261e5f333838685e46d6dc6
임성애 <tjddo2791@naver.com>
2021-07-28 17:47:21 +0900

    [S05P12C107-69] [FE-seongaeee] feat: 로컬 server로 로그인 API test 환경 구축

--- frontend/mabcci/src/API/AuthAPI/LoginApi.js
+++ frontend/mabcci/src/API/AuthAPI/LoginApi.js
@@ -3,41 +3,37 @@ import jwtDecode from 'jwt-decode';
 import { LoginUrl } from '../ApiUrl';
 
 const LoginApi = async userAuthInfo => {
-  await axios
-    .post(LoginUrl, userAuthInfo, {
+  try {
+    const response = await axios.post(LoginUrl, userAuthInfo, {
       headers: {
         'Content-Type': 'application/json',
       },
-    })
-    .then(response => {
-      const { accessToken } = response;
-      const { refreshToken } = response;
-      const decoded = jwtDecode(accessToken);
-
-      const userInfo = {
-        email: decoded.email,
-        nickname: decoded.nickname,
-        role: decoded.role,
-      };
-
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
-      localStorage.setItem('userInfo', userInfo);
-
-      return {
-        status: response.status,
-        userInfo,
-      };
-    })
-    .catch(response => {
-      localStorage.removeItem('accessToken');
-      localStorage.removeItem('refreshToken');
-      localStorage.removeItem('userinfo');
-
-      return {
-        status: response.status,
-      };
     });
+
+    const { accessToken } = response.data;
+    const { refreshToken } = response.data;
+    const decoded = jwtDecode(accessToken);
+    const userInfo = {
+      email: decoded.email,
+    };
+
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('userInfo', userInfo);
+
+    return {
+      status: response.status,
+      userInfo,
+    };
+  } catch (response) {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('userinfo');
+
+    return {
+      status: response.status,
+    };
+  }
 };
 
 export default LoginApi;
