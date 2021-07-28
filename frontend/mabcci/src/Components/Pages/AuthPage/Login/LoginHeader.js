70f676191c363af961caf20bdfb60b2265f8b296
임성애 <tjddo2791@naver.com>
2021-07-28 14:40:21 +0900

    [S05P12C107-27] [FE-seongaeee] feat: OOTD Detail 페이지 구조

--- frontend/mabcci/src/Components/Pages/AuthPage/Login/LoginHeader.js
+++ frontend/mabcci/src/Components/Pages/AuthPage/Login/LoginHeader.js
@@ -2,13 +2,8 @@ import React from 'react';
 
 const LoginHeader = () => {
   return (
-<<<<<<< HEAD
     <header className="container-item">
       <h3>로그인</h3>
-=======
-    <header id="container-item">
-      <h3 className="title">로그인</h3>
->>>>>>> 4dede7c ([S05P12C107-69] [FE-seongaeee] feat: 로그인 API 구조 수정)
     </header>
   );
 };
