d4f3f9c2f848c71277f118a62b2f6cefeb1328fe
김성규 <kskyu610@gmail.com>
2021-07-28 23:36:27 +0900

    [S05P12C107-78] [BE-kskyu610] test: JwtToken 생성 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/auth/domain/vo/JwtTokenTest.java
@@ -0,0 +1,33 @@
+package com.mabcci.domain.auth.domain.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class JwtTokenTest {
+
+    @DisplayName(value = "JwtToken 인스턴스 생성 여부 테스트")
+    @Test
+    public void constructor_test() {
+        final String value = "test.jwt.token";
+        final JwtToken jwtToken = JwtToken.of(value);
+
+        assertAll(
+                () -> assertThat(jwtToken).isNotNull(),
+                () -> assertThat(jwtToken).isExactlyInstanceOf(JwtToken.class)
+        );
+    }
+
+    @DisplayName(value = "JwtToken 인스턴스 디폴트 생성자를 이용한 생성 여부 테스트")
+    @Test
+    public void default_constructor_test() {
+        final JwtToken jwtToken = new JwtToken();
+
+        assertAll(
+                () -> assertThat(jwtToken).isNotNull(),
+                () -> assertThat(jwtToken).isExactlyInstanceOf(JwtToken.class)
+        );
+    }
+}
