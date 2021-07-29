7604d9bf3aa15eff2cbc2517cbca0c25eef88486
김성규 <kskyu610@gmail.com>
2021-07-29 13:24:01 +0900

    [S05P12C107-78] [BE-kskyu610] feat: JwtToken 클래스 equals, hashCode 오버라이딩 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/auth/domain/vo/JwtToken.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/auth/domain/vo/JwtToken.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class JwtToken implements Serializable {
@@ -28,5 +29,18 @@ public class JwtToken implements Serializable {
     @JsonValue
     public String jwtToken() {
         return jwtToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JwtToken jwtToken1 = (JwtToken) o;
+        return Objects.equals(jwtToken, jwtToken1.jwtToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(jwtToken);
     }
 }
