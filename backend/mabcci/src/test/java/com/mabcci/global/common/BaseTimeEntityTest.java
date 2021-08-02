7f6214e56cf1acd67a8efcf1df23350113df7e19
김우재 <kwj1270@naver.com>
2021-08-02 14:40:57 +0900

    [S05P12C107-25] [BE-kwj1270] test: BaseTimeEntity 인스턴스 getter 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/global/common/BaseTimeEntityTest.java
+++ backend/mabcci/src/test/java/com/mabcci/global/common/BaseTimeEntityTest.java
@@ -2,6 +2,9 @@ package com.mabcci.global.common;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +20,20 @@ class BaseTimeEntityTest {
         assertAll(
                 () -> assertThat(baseTimeEntity).isNotNull(),
                 () -> assertThat(baseTimeEntity).isExactlyInstanceOf(BaseTimeEntity.class)
+        );
+    }
+
+    @DisplayName("BaseTimeEntity 인스턴스 getter 기능 테스트")
+    @Test
+    void getter_test() {
+        final LocalDateTime now = LocalDateTime.now();
+        final BaseTimeEntity baseTimeEntity = new BaseTimeEntity();
+        ReflectionTestUtils.setField(baseTimeEntity, "createdDate", now);
+        ReflectionTestUtils.setField(baseTimeEntity, "modifiedDate", now);
+
+        assertAll(
+                () -> assertThat(baseTimeEntity.createdDate()).isEqualTo(now),
+                () -> assertThat(baseTimeEntity.modifiedDate()).isEqualTo(now)
         );
     }
 }
