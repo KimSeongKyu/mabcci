43d69d5d98d8fb374e27717a003236e4079db4f1
김우재 <kwj1270@naver.com>
2021-08-02 10:04:36 +0900

    [S05P12C107-25] [BE-kwj1270] feat: Category 인스턴스 생성자를 이용한 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/category/domain/Category.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/category/domain/Category.java
@@ -1,4 +1,23 @@
 package com.mabcci.domain.category.domain;
 
-public class Category {
+import com.mabcci.global.common.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Entity
+public class Category extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    protected Category() {
+    }
+
+    public Category(final String categoryName) {
+        this.categoryName = categoryName;
+    }
+
 }
