0c98b861f1fe5e5a22ce0425dd1bf5cbf64d2662
김성규 <kskyu610@gmail.com>
2021-08-02 11:22:57 +0900

    [S05P13C107-74] [BE-kskyu610] test: Ootd 클래스 테스트용 인스턴스를 멤버 변수로 설정

--- backend/mabcci/src/test/java/com/mabcci/domain/ootd/domain/OotdTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootd/domain/OotdTest.java
@@ -1,5 +1,6 @@
 package com.mabcci.domain.ootd.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class OotdTest {
 
-    @DisplayName("Ootd 인스턴스 생성 여부 테스트")
-    @Test
-    void initialize() {
-        final Ootd ootd = Ootd.builder()
+    private Ootd ootd;
+
+    @BeforeEach
+    void setUp() {
+        ootd = Ootd.builder()
                 .member(MEMBER)
                 .content("content")
                 .top("top")
@@ -20,7 +22,11 @@ class OotdTest {
                 .shoes("shoes")
                 .accessory("accessory")
                 .build();
+    }
 
+    @DisplayName("Ootd 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
         assertAll(
                 () -> assertThat(ootd).isNotNull(),
                 () -> assertThat(ootd).isExactlyInstanceOf(Ootd.class)
