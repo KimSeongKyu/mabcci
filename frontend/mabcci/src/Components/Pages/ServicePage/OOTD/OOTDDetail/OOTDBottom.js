dfcd77d1fa0460b8e3919242e9c82eb2475698b0
임성애 <tjddo2791@naver.com>
2021-08-04 01:29:06 +0900

    [S05P13C107-37] [FE-seongaeee] fix: OOTD 상세 조회 댓글 보내기 디자인 추가

--- frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
+++ frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoMdSend } from 'react-icons/io';
 import userphoto from './Images/userphoto.png';
 
 const OOTDBottom = () => {
@@ -27,6 +28,14 @@ const OOTDBottom = () => {
             adipiscing elit, sed do eiusmod tempor incididunt
           </div>
         </div>
+      </div>
+      <div className="detail-comment-write">
+        <input
+          className="detail-comment-input"
+          type="text"
+          placeholder="댓글 쓰기"
+        />
+        <IoMdSend className="detail-comment-send" size="30" />
       </div>
     </footer>
   );
