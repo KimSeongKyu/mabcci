847c96b470be476ca359b0d5b6f52cf912e8dfa8
김성규 <kskyu610@gmail.com>
2021-08-03 15:52:00 +0900

    [S05P13C107-81] [BE-kskyu610] test: PictureUtil 인스턴스 전달받은 사진이 없는 경우 빈 리스트를 반환하는 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/global/common/PictureUtilTest.java
+++ backend/mabcci/src/test/java/com/mabcci/global/common/PictureUtilTest.java
@@ -3,6 +3,10 @@ package com.mabcci.global.common;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -23,5 +27,14 @@ class PictureUtilTest {
                 () -> assertThat(pictureUtil).isNotNull(),
                 () -> assertThat(pictureUtil).isExactlyInstanceOf(PictureUtil.class)
         );
+    }
+
+    @DisplayName("PictureUtil 인스턴스 전달받은 사진이 없는 경우 빈 리스트를 반환하는 테스트")
+    @Test
+    void return_empty_list_when_no_picture() {
+        List<MultipartFile> pictures = new ArrayList<>();
+        List<Picture> pictureEntities = pictureUtil.toEntities(pictures);
+
+        assertThat(pictureEntities).isEmpty();
     }
 }
