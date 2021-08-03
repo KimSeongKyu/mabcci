30f68a4ec5ee68c4270848e72d248036be02d86c
김성규 <kskyu610@gmail.com>
2021-08-03 11:08:47 +0900

    [S05P13C107-80] [BE-kskyu610] feat: OotdPictureRepository 인터페이스 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/domain/OotdPictureRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/domain/OotdPictureRepositoryTest.java
@@ -3,10 +3,14 @@ package com.mabcci.domain.ootdpicture.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+@EnableJpaAuditing
+@DataJpaTest
 class OotdPictureRepositoryTest {
 
     @Autowired
