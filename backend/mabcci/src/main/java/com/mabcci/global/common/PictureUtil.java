8688c9cd9bcec1f8770fc1237075d50b9eb1671b
김성규 <kskyu610@gmail.com>
2021-08-03 15:53:27 +0900

    [S05P13C107-81] [BE-kskyu610] feat: PictureUtil 인스턴스 전달받은 사진이 없는 경우 빈 리스트를 반환하는 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/global/common/PictureUtil.java
+++ backend/mabcci/src/main/java/com/mabcci/global/common/PictureUtil.java
@@ -1,10 +1,24 @@
 package com.mabcci.global.common;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class PictureUtil {
 
     public PictureUtil() {
+    }
+
+    public List<Picture> toEntities(final List<MultipartFile> pictures) {
+        List<Picture> entities = new ArrayList<>();
+
+        if(!pictures.isEmpty()) {
+            entities.add(new Picture());
+        }
+
+        return entities;
     }
 }
