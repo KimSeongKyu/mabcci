851735636c9460273373e81cbe7a0a2cdda78edd
김성규 <kskyu610@gmail.com>
2021-08-03 22:27:06 +0900

    [S05P13C107-81] [BE-kskyu610] feat: PictureUtil 인스턴스 확장자명 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/global/common/PictureUtil.java
+++ backend/mabcci/src/main/java/com/mabcci/global/common/PictureUtil.java
@@ -1,5 +1,6 @@
 package com.mabcci.global.common;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +15,8 @@ public class PictureUtil {
 
     private final static String TIME_FORMAT = "yyyyMMdd";
     private final static String IMAGES_DIRECTORY_NAME = "images";
+    private final static String PNG_FILE_EXTENSION = ".png";
+    private final static String JPG_FILE_EXTENSION = ".jpg";
 
     public List<Picture> toEntities(final List<MultipartFile> pictures) {
         List<Picture> entities = new ArrayList<>();
@@ -29,5 +32,12 @@ public class PictureUtil {
         final String todayFormedToTimeFormat = LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern(TIME_FORMAT));
         return IMAGES_DIRECTORY_NAME + File.separator + todayFormedToTimeFormat;
+    }
+
+    public String makeOriginalFileExtension(final String contentType) {
+        if (contentType.contains(MediaType.IMAGE_PNG_VALUE)) {
+            return PNG_FILE_EXTENSION;
+        }
+        return JPG_FILE_EXTENSION;
     }
 }
