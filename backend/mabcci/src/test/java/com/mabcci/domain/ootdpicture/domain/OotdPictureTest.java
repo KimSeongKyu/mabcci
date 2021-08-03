3fc912e714d238a2a3e19b48650934c867b4d8c6
김성규 <kskyu610@gmail.com>
2021-08-03 10:55:53 +0900

    [S05P13C107-80] [BE-kskyu610] test: OotdPicture 인스턴스 getter 메서드들 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/domain/OotdPictureTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/domain/OotdPictureTest.java
@@ -41,4 +41,15 @@ class OotdPictureTest {
                 () -> assertThat(ootdPicture).isExactlyInstanceOf(OotdPicture.class)
         );
     }
+
+    @DisplayName("OotdPicture 인스턴스 getter 메서드들 테스트")
+    @Test
+    void getter_test() {
+        assertAll(
+                () -> assertThat(ootdPicture.id()).isEqualTo(1L),
+                () -> assertThat(ootdPicture.ootd()).isEqualTo(OOTD),
+                () -> assertThat(ootdPicture.url()).isEqualTo("testUrl"),
+                () -> assertThat(ootdPicture.fileName()).isEqualTo("testFileName")
+        );
+    }
 }
