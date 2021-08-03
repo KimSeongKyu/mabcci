c83094be8be410f4fe9a437043cab600fab3f91f
김성규 <kskyu610@gmail.com>
2021-08-03 16:49:08 +0900

    [S05P13C107-81] [BE-kskyu610] feat: PictureUtil 인스턴스 디렉토리 이름 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/global/common/PictureUtil.java
+++ backend/mabcci/src/main/java/com/mabcci/global/common/PictureUtil.java
@@ -3,22 +3,31 @@ package com.mabcci.global.common;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PictureUtil {
 
-    public PictureUtil() {
-    }
+    private final static String TIME_FORMAT = "yyyyMMdd";
+    private final static String IMAGES_DIRECTORY_NAME = "images";
 
     public List<Picture> toEntities(final List<MultipartFile> pictures) {
         List<Picture> entities = new ArrayList<>();
 
-        if(!pictures.isEmpty()) {
+        if (!pictures.isEmpty()) {
             entities.add(new Picture());
         }
 
         return entities;
+    }
+
+    public String makeDirectoryName() {
+        final String todayFormedToTimeFormat = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern(TIME_FORMAT));
+        return IMAGES_DIRECTORY_NAME + File.separator + todayFormedToTimeFormat;
     }
 }
