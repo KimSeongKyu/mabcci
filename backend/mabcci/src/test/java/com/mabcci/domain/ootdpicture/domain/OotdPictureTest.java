eb4c3f295078359f1d73ea943ede4221ce0453ac
김성규 <kskyu610@gmail.com>
2021-08-03 10:53:45 +0900

    [S05P13C107-80] [BE-kskyu610] test: OotdPicture 인스턴스 디폴트 생성자를 이용한 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/domain/OotdPictureTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/domain/OotdPictureTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.Validation;
 import static com.mabcci.domain.ootd.domain.OotdTest.OOTD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -24,6 +25,17 @@ class OotdPictureTest {
     @DisplayName("OotdPicture 인스턴스 생성 여부 테스트")
     @Test
     void initialize() {
+        assertAll(
+                () -> assertThat(ootdPicture).isNotNull(),
+                () -> assertThat(ootdPicture).isExactlyInstanceOf(OotdPicture.class)
+        );
+    }
+
+    @DisplayName("OotdPicture 인스턴스 디폴트 생성자를 이용한 생성 테스트")
+    @Test
+    void default_constructor_test() {
+        final OotdPicture ootdPicture = new OotdPicture();
+
         assertAll(
                 () -> assertThat(ootdPicture).isNotNull(),
                 () -> assertThat(ootdPicture).isExactlyInstanceOf(OotdPicture.class)
