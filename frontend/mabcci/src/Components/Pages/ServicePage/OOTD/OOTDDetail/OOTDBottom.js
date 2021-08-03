d3b6ed1cfdc8c432088c029ea20697052c1ea201
임성애 <tjddo2791@naver.com>
2021-08-03 16:24:54 +0900

    [S05P13C107-37] [FE-seongaeee] feat: OOTD 상세 조회 댓글 디자인 구조 구축

--- frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
+++ frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import userphoto from './Images/userphoto.png';
 
 const OOTDBottom = () => {
   return (
     <footer className="detail-bottom">
-      <div className="detail-bottom-title">
+      <div className="detail-comments-title">
         <h4>Comments (3)</h4>
+      </div>
+      <div className="detail-comments">
+        <img src={userphoto} alt="UserImage" width="70" />
       </div>
     </footer>
   );
