0bed13fd2b125e7dd006968dceb68d750f5acce0
김성규 <kskyu610@gmail.com>
2021-08-04 16:39:37 +0900

    [S05P13C107-84] [BE-kskyu610] test: HahstagRegisterResponse 인스턴스 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/hashtag/dto/HashtagRegisterResponseTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/dto/HashtagRegisterResponseTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -42,5 +46,22 @@ class HashtagRegisterResponseTest {
     @Test
     void getter_test() {
         assertThat(hashtagRegisterResponse.getHashtags()).isEqualTo(hashtags);
+    }
+
+    @DisplayName("HashtagRegisterResponse 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final HashtagRegisterResponse invalidResponse = new HashtagRegisterResponse(new ArrayList<>());
+
+        final Set<ConstraintViolation<HashtagRegisterResponse>> invalidPropertiesOfValidResponse =
+                validator.validate(hashtagRegisterResponse);
+        final Set<ConstraintViolation<HashtagRegisterResponse>> invalidPropertiesOfInvalidResponse =
+                validator.validate(invalidResponse);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidResponse.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidResponse.size()).isEqualTo(1)
+        );
     }
 }
