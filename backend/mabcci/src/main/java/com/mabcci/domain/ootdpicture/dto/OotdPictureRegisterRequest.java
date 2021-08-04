e71342540b6b016379227e87f6dfc2784ee8c1eb
김성규 <kskyu610@gmail.com>
2021-08-04 14:15:49 +0900

    [S05P13C107-81] [BE-kskyu610] test: OotdPictureRegisterRequest 인스턴스 프로퍼티 유효성 검사 테스트 작성

--- backend/mabcci/src/main/java/com/mabcci/domain/ootdpicture/dto/OotdPictureRegisterRequest.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdpicture/dto/OotdPictureRegisterRequest.java
@@ -2,13 +2,19 @@ package com.mabcci.domain.ootdpicture.dto;
 
 import com.mabcci.domain.picture.domain.Picture;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 public final class OotdPictureRegisterRequest {
 
+    @NotEmpty
     private List<Picture> pictures;
 
     public OotdPictureRegisterRequest(final List<Picture> pictures) {
         this.pictures = pictures;
+    }
+
+    public final List<Picture> getPictures() {
+        return pictures;
     }
 }
--- backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/dto/OotdPictureRegisterRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdpicture/dto/OotdPictureRegisterRequestTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -38,5 +42,22 @@ class OotdPictureRegisterRequestTest {
     @Test
     void getter_test() {
         assertThat(ootdPictureRegisterRequest.getPictures()).isEqualTo(pictures);
+    }
+
+    @DisplayName("OOtdPictureRegisterRequest 인스턴스 프로퍼티 유효성 검사 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdPictureRegisterRequest invalidRequest = new OotdPictureRegisterRequest(new ArrayList<>());
+
+        final Set<ConstraintViolation<OotdPictureRegisterRequest>> constraintViolationsOfValidRequest =
+                validator.validate(ootdPictureRegisterRequest);
+        final Set<ConstraintViolation<OotdPictureRegisterRequest>> constraintViolationsOfInvalidRequest =
+                validator.validate(invalidRequest);
+
+        assertAll(
+                () -> assertThat(constraintViolationsOfValidRequest.size()).isEqualTo(0),
+                () -> assertThat(constraintViolationsOfInvalidRequest.size()).isEqualTo(1)
+        );
     }
 }
