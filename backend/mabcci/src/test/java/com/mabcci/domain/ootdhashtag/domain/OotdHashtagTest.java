dae988300d6a5d187bb4026fdca5fe9369b941c0
김성규 <kskyu610@gmail.com>
2021-08-04 22:48:14 +0900

    [S05P13C107-84] [BE-kskyu610] test: OotdHashtag 인스턴스 getter 메서드들 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/domain/OotdHashtagTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/domain/OotdHashtagTest.java
@@ -3,6 +3,7 @@ package com.mabcci.domain.ootdhashtag.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static com.mabcci.domain.hashtag.domain.HashtagTest.HASHTAG;
 import static com.mabcci.domain.ootd.domain.OotdTest.OOTD;
@@ -38,6 +39,18 @@ class OotdHashtagTest {
         assertAll(
                 () -> assertThat(ootdHashtag).isNotNull(),
                 () -> assertThat(ootdHashtag).isExactlyInstanceOf(OotdHashtag.class)
+        );
+    }
+
+    @DisplayName("OotdHashtag 인스턴스 getter 메서드들 테스트")
+    @Test
+    void getter_test() {
+        ReflectionTestUtils.setField(ootdHashtag, "id", 1L);
+
+        assertAll(
+                () -> assertThat(ootdHashtag.id()).isEqualTo(1L),
+                () -> assertThat(ootdHashtag.ootd()).isEqualTo(OOTD),
+                () -> assertThat(ootdHashtag.hashtag()).isEqualTo(HASHTAG)
         );
     }
 }
