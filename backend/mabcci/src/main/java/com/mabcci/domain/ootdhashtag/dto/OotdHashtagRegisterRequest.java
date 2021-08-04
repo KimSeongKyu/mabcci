a7032c975c785fad654e602c1719d0d83edf0727
김성규 <kskyu610@gmail.com>
2021-08-04 23:18:40 +0900

    [S05P13C107-84] [BE-kskyu610] feat: OotdHashtagRegisterRequest 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdhashtag/dto/OotdHashtagRegisterRequest.java
@@ -0,0 +1,25 @@
+package com.mabcci.domain.ootdhashtag.dto;
+
+import com.mabcci.domain.hashtag.domain.Hashtag;
+import com.mabcci.domain.ootd.domain.Ootd;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public final class OotdHashtagRegisterRequest {
+
+    @NotNull
+    private Ootd ootd;
+
+    @NotEmpty
+    private List<Hashtag> hashtags;
+
+    private OotdHashtagRegisterRequest() {
+    }
+
+    public OotdHashtagRegisterRequest(final Ootd ootd, final List<Hashtag> hashtags) {
+        this.ootd = ootd;
+        this.hashtags = hashtags;
+    }
+}
