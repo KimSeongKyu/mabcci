9760c3683b551d6493b8b7d8822d55c4ad54ff4e
김우재 <kwj1270@naver.com>
2021-08-04 10:38:39 +0900

    [S05P12C107-25] [BE-kwj1270] feat: Follow 인스턴스 기본 생성자를 이용한 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/follow/domain/Follow.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/follow/domain/Follow.java
@@ -1,4 +1,25 @@
 package com.mabcci.domain.follow.domain;
 
-public class Follow {
+import com.mabcci.domain.BaseTimeEntity;
+import com.mabcci.domain.member.domain.Member;
+
+import javax.persistence.*;
+
+@Entity
+public class Follow extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following")
+    private Member following;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower")
+    private Member follower;
+
+    protected Follow() { }
+
 }
