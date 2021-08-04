1704a812282f2c1d90b19bccff4160b85812946c
임성애 <tjddo2791@naver.com>
2021-08-04 13:06:21 +0900

    [S05P13C107-37] [FE-seongaeee] fix: OOTD 상세 조회 좋아요 및 수정 삭제 버튼 추가

--- frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
+++ frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
@@ -2,6 +2,30 @@ import React from 'react';
 import { IoMdSend } from 'react-icons/io';
 import userphoto from './Images/userphoto.png';
 
+export const SingleComment = () => {
+  return (
+    <div className="detail-comment">
+      <div className="detail-comment-info">
+        <div className="detail-comment-info-photo">
+          <img src={userphoto} alt="UserImage" width="70" />
+        </div>
+        <div className="detail-comment-info-content">
+          <p>Username</p>
+          <p>5일</p>
+          <button type="button">답글 달기</button>
+          <button type="button">수정</button>
+          <button type="button">삭제</button>
+        </div>
+      </div>
+      <div className="detail-comment-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt
+      </div>
+    </div>
+  );
+};
+
 const OOTDBottom = () => {
   return (
     <footer className="detail-bottom">
@@ -9,25 +33,7 @@ const OOTDBottom = () => {
         <h5>Comments (3)</h5>
       </div>
       <div className="detail-comments">
-        <div className="detail-comment">
-          <div className="detail-comment-info">
-            <div className="detail-comment-info-photo">
-              <img src={userphoto} alt="UserImage" width="70" />
-            </div>
-            <div className="detail-comment-info-content">
-              <p>Username</p>
-              <p>5일</p>
-              <button type="button">답글 달기</button>
-              <button type="button">수정</button>
-              <button type="button">삭제</button>
-            </div>
-          </div>
-          <div className="detail-comment-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor incididunt
-          </div>
-        </div>
+        <SingleComment />
       </div>
       <div className="detail-comment-write">
         <input
