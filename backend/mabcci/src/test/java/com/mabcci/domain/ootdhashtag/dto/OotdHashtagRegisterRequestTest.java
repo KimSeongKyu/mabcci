0048ebacf918c2c54f2629b4bcbfdb16b216c736
김성규 <kskyu610@gmail.com>
2021-08-04 23:29:28 +0900

    [S05P13C107-84] [BE-kskyu610] test: OotdHashtagRegisterRequest 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/dto/OotdHashtagRegisterRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/dto/OotdHashtagRegisterRequestTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static com.mabcci.domain.hashtag.domain.HashtagTest.HASHTAG;
 import static com.mabcci.domain.ootd.domain.OotdTest.OOTD;
@@ -40,5 +44,23 @@ class OotdHashtagRegisterRequestTest {
                 () -> assertThat(ootdHashtagRegisterRequest.getOotd()).isEqualTo(OOTD),
                 () -> assertThat(ootdHashtagRegisterRequest.getHashtags()).isEqualTo(hashtags)
         );
+    }
+
+    @DisplayName("OotdHashtagRegisterRequest 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdHashtagRegisterRequest invalidRequest = new OotdHashtagRegisterRequest(null, new ArrayList<>());
+
+        final Set<ConstraintViolation<OotdHashtagRegisterRequest>> invalidPropertiesOfValidRequest =
+                validator.validate(ootdHashtagRegisterRequest);
+        final Set<ConstraintViolation<OotdHashtagRegisterRequest>> invalidPropertiesOfInValidRequest =
+                validator.validate(invalidRequest);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidRequest.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInValidRequest.size()).isEqualTo(2)
+        );
+
     }
 }
