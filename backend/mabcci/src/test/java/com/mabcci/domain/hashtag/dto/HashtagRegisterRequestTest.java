7652a0ed1e82e4c711b3727c5cb381f2cc74f4c3
김성규 <kskyu610@gmail.com>
2021-08-04 16:18:28 +0900

    [S05P13C107-84] [BE-kskyu610] test: HahstagRegisterRequest 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/dto/HashtagRegisterRequestTest.java
@@ -0,0 +1,33 @@
+package com.mabcci.domain.hashtag.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class HashtagRegisterRequestTest {
+
+    private HashtagRegisterRequest hashtagRegisterRequest;
+
+    @BeforeEach
+    void setUp() {
+        List<String> names = new ArrayList<>(List.of(
+                "해시태그1", "해시태그2"
+        ));
+        hashtagRegisterRequest = new HashtagRegisterRequest(names);
+    }
+
+    @DisplayName("HashtagRegisterRequest 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(hashtagRegisterRequest).isNotNull(),
+                () -> assertThat(hashtagRegisterRequest).isExactlyInstanceOf(HashtagRegisterRequest.class)
+        );
+    }
+}
