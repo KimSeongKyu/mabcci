e58f1f0d04d64fa8130f77e0ed653fb5893fbf3b
김성규 <kskyu610@gmail.com>
2021-08-04 16:32:17 +0900

    [S05P13C107-84] [BE-kskyu610] test: HahstagRegisterResponse 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/dto/HashtagRegisterResponseTest.java
@@ -0,0 +1,40 @@
+package com.mabcci.domain.hashtag.dto;
+
+import com.mabcci.domain.hashtag.domain.Hashtag;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class HashtagRegisterResponseTest {
+
+    private HashtagRegisterResponse hashtagRegisterResponse;
+    private List<Hashtag> hashtags;
+
+    @BeforeEach
+    void setUp() {
+        hashtags = new ArrayList<>(List.of(
+                Hashtag.builder()
+                        .name("해시태그1")
+                        .build(),
+                Hashtag.builder()
+                        .name("해시태그2")
+                        .build()
+        ));
+        hashtagRegisterResponse = new HashtagRegisterResponse(hashtags);
+    }
+
+    @DisplayName("HashtagRegisterResponse 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(hashtagRegisterResponse).isNotNull(),
+                () -> assertThat(hashtagRegisterResponse).isExactlyInstanceOf(HashtagRegisterResponse.class)
+        );
+    }
+}
