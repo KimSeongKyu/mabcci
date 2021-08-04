32f25291d43009bccc9d882f637e46ea6232f1cf
임성애 <tjddo2791@naver.com>
2021-08-04 13:22:37 +0900

    [S05P13C107-37] [FE-seongaeee] feat: OOTD 상세 조회 업데이트 이벤트 추가

--- frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
+++ frontend/mabcci/src/Components/Pages/ServicePage/OOTD/OOTDDetail/OOTDBottom.js
@@ -5,10 +5,28 @@ const OOTDBottom = () => {
   return (
     <footer className="detail-bottom">
       <div className="detail-comments-title">
-        <h4>Comments (3)</h4>
+        <h5>Comments (3)</h5>
       </div>
       <div className="detail-comments">
-        <img src={userphoto} alt="UserImage" width="70" />
+        <div className="detail-comment">
+          <div className="detail-comment-info">
+            <div className="detail-comment-info-photo">
+              <img src={userphoto} alt="UserImage" width="70" />
+            </div>
+            <div className="detail-comment-info-content">
+              <p>Username</p>
+              <p>5일</p>
+              <button type="button">답글 달기</button>
+              <button type="button">수정</button>
+              <button type="button">삭제</button>
+            </div>
+          </div>
+          <div className="detail-comment-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt
+          </div>
+        </div>
       </div>
     </footer>
   );
