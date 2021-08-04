d2fb05f6d0e395cf3655b673b58dc2e105285550
김성규 <kskyu610@gmail.com>
2021-08-04 15:57:55 +0900

    [S05P13C107-84] [BE-kskyu610] test: HahstagRepository 구현체 존재 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/domain/HashtagRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.mabcci.domain.hashtag.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@EnableJpaAuditing
+@DataJpaTest
+class HashtagRepositoryTest {
+
+    @Autowired
+    private HashtagRepository hashtagRepository;
+
+    @DisplayName("HashtagRepository 구현체 존재 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(hashtagRepository).isNotNull(),
+                () -> assertThat(hashtagRepository).isInstanceOf(HashtagRepository.class)
+        );
+    }
+}
