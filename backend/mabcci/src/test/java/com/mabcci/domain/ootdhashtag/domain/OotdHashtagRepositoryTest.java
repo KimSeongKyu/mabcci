6091248950aebc002f45ca200085f1a541a138ca
김성규 <kskyu610@gmail.com>
2021-08-04 22:45:48 +0900

    [S05P13C107-84] [BE-kskyu610] test: OotdHashtagRepository save 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/domain/OotdHashtagRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/domain/OotdHashtagRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.mabcci.domain.ootdhashtag.domain;
 
+import com.mabcci.domain.hashtag.domain.Hashtag;
+import com.mabcci.domain.ootd.domain.Ootd;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import static com.mabcci.domain.member.domain.MemberTest.MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -16,6 +21,35 @@ public class OotdHashtagRepositoryTest {
     @Autowired
     private OotdHashtagRepository ootdHashtagRepository;
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    private OotdHashtag ootdHashtag;
+    private Ootd ootd;
+    private Hashtag hashtag;
+
+    @BeforeEach
+    void setUp() {
+        ootd = Ootd.builder()
+                .member(MEMBER)
+                .content("content")
+                .top("top")
+                .bottom("bottom")
+                .shoes("shoes")
+                .accessory("accessory")
+                .views(0L)
+                .build();
+
+        hashtag = Hashtag.builder()
+                .name("해시태그")
+                .build();
+
+        ootdHashtag = OotdHashtag.builder()
+                .ootd(ootd)
+                .hashtag(hashtag)
+                .build();
+    }
+
     @DisplayName("OotdHashtagRepository 구현체 존재 여부 테스트")
     @Test
     void initialize() {
@@ -23,5 +57,16 @@ public class OotdHashtagRepositoryTest {
                 () -> assertThat(ootdHashtagRepository).isNotNull(),
                 () -> assertThat(ootdHashtagRepository).isInstanceOf(OotdHashtagRepository.class)
         );
+    }
+
+    @DisplayName("OotdHashtagRepository save 기능 테스트")
+    @Test
+    void save_test() {
+        testEntityManager.persist(ootd);
+        testEntityManager.persist(hashtag);
+
+        final OotdHashtag savedOotdHashtag = ootdHashtagRepository.save(ootdHashtag);
+
+        assertThat(savedOotdHashtag.id()).isEqualTo(ootdHashtag.id());
     }
 }
