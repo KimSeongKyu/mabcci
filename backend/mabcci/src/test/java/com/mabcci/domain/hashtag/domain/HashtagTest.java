b7f3955c68b275a2ba641775c134f60499564d68
김성규 <kskyu610@gmail.com>
2021-08-04 15:38:24 +0900

    [S05P13C107-84] [BE-kskyu610] test: Hahstag 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/domain/HashtagTest.java
@@ -0,0 +1,29 @@
+package com.mabcci.domain.hashtag.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class HashtagTest {
+
+    private Hashtag hashtag;
+
+    @BeforeEach
+    void setUp() {
+        hashtag = Hashtag.builder
+                .name("해시태그")
+                .build();
+    }
+
+    @DisplayName("Hashtag 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(hashtag).isNotNull(),
+                () -> assertThat(hashtag).isExactlyInstanceOf(Hashtag.class)
+        );
+    }
+}
