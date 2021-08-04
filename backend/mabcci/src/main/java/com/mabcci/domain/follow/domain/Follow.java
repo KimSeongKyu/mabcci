9371bc62102a723c294f34de6943b8d454e75aa5
김우재 <kwj1270@naver.com>
2021-08-04 10:52:08 +0900

    [S05P12C107-25] [BE-kwj1270] test: Follow 인스턴스 빌더를 이용한 생성 테스트 작성

--- backend/mabcci/src/main/java/com/mabcci/domain/follow/domain/Follow.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/follow/domain/Follow.java
@@ -4,6 +4,7 @@ import com.mabcci.domain.BaseTimeEntity;
 import com.mabcci.domain.member.domain.Member;
 
 import javax.persistence.*;
+import java.text.BreakIterator;
 
 @Entity
 public class Follow extends BaseTimeEntity {
@@ -22,4 +23,36 @@ public class Follow extends BaseTimeEntity {
 
     protected Follow() { }
 
+    protected Follow(final FollowBuilder followBuilder) {
+        this.following = followBuilder.following;
+        this.follower = followBuilder.follower;
+    }
+
+    public static FollowBuilder Builder() {
+        return new FollowBuilder();
+    }
+
+    public static class FollowBuilder {
+        private Member following;
+        private Member follower;
+
+        private FollowBuilder(){
+
+        }
+
+        public FollowBuilder following(final Member following) {
+            this.following = following;
+            return this;
+        }
+
+        public FollowBuilder follower(final Member follower) {
+            this.follower = follower;
+            return this;
+        }
+
+        public Follow build() {
+            return new Follow(this);
+        }
+
+    }
 }
--- backend/mabcci/src/test/java/com/mabcci/domain/follow/domain/FollowTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/follow/domain/FollowTest.java
@@ -1,8 +1,19 @@
 package com.mabcci.domain.follow.domain;
 
+import com.mabcci.domain.member.domain.BodyType;
+import com.mabcci.domain.member.domain.Member;
+import com.mabcci.domain.member.domain.MemberSpecs;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import static com.mabcci.domain.member.domain.Gender.MALE;
+import static com.mabcci.domain.member.domain.MemberRole.USER;
+import static com.mabcci.domain.member.domain.MemberSpecsTest.*;
+import static com.mabcci.global.common.EmailTest.EMAIL;
+import static com.mabcci.global.common.NicknameTest.NICKNAME;
+import static com.mabcci.global.common.PasswordTest.PASSWORD;
+import static com.mabcci.global.common.PhoneTest.PHONE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,12 +21,51 @@ class FollowTest {
 
     @DisplayName("Follow 인스턴스 기본 생성자를 이용한 생성 테스트")
     @Test
-    void constructor_test() {
+    void default_constructor_test() {
         final Follow follow = new Follow();
 
         assertAll(
                 () -> assertThat(follow).isNotNull(),
                 () -> assertThat(follow).isExactlyInstanceOf(Follow.class)
         );
+    }
+
+    @DisplayName("Follow 인스턴스 빌더를 이용한 생성 테스트")
+    @Test
+    void builder_test() {
+        final Member following = member();
+        final Member follower = member();
+        ReflectionTestUtils.setField(follower, "id", 1L);
+        ReflectionTestUtils.setField(following, "id", 2L);
+
+        final Follow follow = follow(following, follower);
+        assertAll(
+                () -> assertThat(follow).isNotNull(),
+                () -> assertThat(follow).isExactlyInstanceOf(Follow.class)
+        );
+    }
+
+    private Follow follow(Member following, Member follower) {
+        return Follow.Builder()
+                .following(following)
+                .follower(follower)
+                .build();
+    }
+
+    private Member member() {
+        return Member.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
+                .phone(PHONE)
+                .gender(MALE)
+                .role(USER)
+                .memberSpecs(MemberSpecs.Build()
+                        .height(HEIGHT)
+                        .weight(WEIGHT)
+                        .footSize(FOOT_SIZE)
+                        .form(BodyType.TRIANGLE)
+                        .build())
+                .build();
     }
 }
