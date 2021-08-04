4a00471fdef76a545f7c04986d4b1cd5ec8e9c1b
김성규 <kskyu610@gmail.com>
2021-08-04 15:58:51 +0900

    [S05P13C107-84] [BE-kskyu610] feat: HahstagRepository 인터페이스 작성

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/hashtag/domain/HashtagRepository.java
@@ -0,0 +1,6 @@
+package com.mabcci.domain.hashtag.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+}
