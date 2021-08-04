90372e6d75fbebe8fc6f78b8b354744e2a5912ec
김성규 <kskyu610@gmail.com>
2021-08-04 11:59:46 +0900

    [S05P13C107-81] [BE-kskyu610] test: PictureRegisterRequest 인스턴스 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/picture/dto/PictureRegisterRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/picture/dto/PictureRegisterRequestTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.ArrayList;
+import java.util.Set;
+
 import static com.mabcci.domain.picture.common.PictureUtilTest.PICTURE_FILES;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -30,5 +36,22 @@ class PictureRegisterRequestTest {
     @Test
     void getter_test() {
         assertThat(pictureRegisterRequest.getPictures()).isEqualTo(PICTURE_FILES);
+    }
+
+    @DisplayName("PictureRegisterRequest 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final PictureRegisterRequest invalidPictureRegisterRequest = new PictureRegisterRequest(new ArrayList<>());
+
+        final Set<ConstraintViolation<PictureRegisterRequest>> invalidPropertiesOfValidPictureRegisterRequest
+                = validator.validate(pictureRegisterRequest);
+        final Set<ConstraintViolation<PictureRegisterRequest>> invalidPropertiesOfInvalidPictureRegisterRequest
+                = validator.validate(invalidPictureRegisterRequest);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidPictureRegisterRequest.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidPictureRegisterRequest.size()).isEqualTo(1)
+        );
     }
 }
