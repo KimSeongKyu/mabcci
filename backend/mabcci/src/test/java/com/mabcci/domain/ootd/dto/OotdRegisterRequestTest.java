957e62ab760b6403b56d977b4e3b3ebd9e15a65f
김성규 <kskyu610@gmail.com>
2021-08-05 00:23:55 +0900

    [S05P13C107-84] [BE-kskyu610] test: OotdRegisterRequest 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootd/dto/OotdRegisterRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootd/dto/OotdRegisterRequestTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static com.mabcci.domain.picture.common.PictureUtilTest.PICTURE_FILES;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,5 +49,24 @@ class OotdRegisterRequestTest {
                 () -> assertThat(ootdRegisterRequest.getPictures()).isEqualTo(PICTURE_FILES),
                 () -> assertThat(ootdRegisterRequest.getHashtags()).isEqualTo(new ArrayList<>(List.of("해시태그1", "해시태그2"))
         ));
+    }
+
+    @DisplayName("OotdRegisterRequest 인스턴스 프토퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdRegisterRequest invalidRequest = new OotdRegisterRequest(
+                null, null, null, null, null, null,
+                new ArrayList<>(), null);
+
+        final Set<ConstraintViolation<OotdRegisterRequest>> invalidPropertiesOfValidRequest =
+                validator.validate(ootdRegisterRequest);
+        final Set<ConstraintViolation<OotdRegisterRequest>> invalidPropertiesOfInvalidRequest =
+                validator.validate(invalidRequest);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidRequest.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidRequest.size()).isEqualTo(2)
+        );
     }
 }
