dd10fc0510a2af12a38ae5005beece479dd01fe1
김우재 <kwj1270@naver.com>
2021-08-04 14:48:31 +0900

    [S05P13C107-83] [BE-kwj1270] feat: FollowService 인스턴스 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/follow/application/FollowService.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/follow/application/FollowService.java
@@ -1,7 +1,15 @@
 package com.mabcci.domain.follow.application;
 
+import com.mabcci.domain.follow.domain.FollowRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowService {
+
+    private final FollowRepository followRepository;
+
+    public FollowService(final FollowRepository followRepository) {
+        this.followRepository = followRepository;
+    }
+
 }
