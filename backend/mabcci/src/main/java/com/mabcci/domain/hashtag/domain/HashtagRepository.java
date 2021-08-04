025a727ab587c4550f377c316520a0139f7352ad
김성규 <kskyu610@gmail.com>
2021-08-04 17:14:22 +0900

    [S05P13C107-84] [BE-kskyu610] feat: HahstagRepository 인터페이스 findByName 추상 메서드 작성

--- backend/mabcci/src/main/java/com/mabcci/domain/hashtag/domain/HashtagRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/hashtag/domain/HashtagRepository.java
@@ -2,5 +2,8 @@ package com.mabcci.domain.hashtag.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+    Optional<Hashtag> findByName(String name);
 }
