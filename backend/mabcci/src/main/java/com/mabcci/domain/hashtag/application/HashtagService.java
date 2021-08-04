007f7bec066626afcec31bea5a3e3a3f1f0fa61f
김성규 <kskyu610@gmail.com>
2021-08-04 17:24:20 +0900

    [S05P13C107-84] [BE-kskyu610] feat: HahstagService 인스턴스 Hashtag 저장 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/hashtag/application/HashtagService.java
@@ -0,0 +1,32 @@
+package com.mabcci.domain.hashtag.application;
+
+import com.mabcci.domain.hashtag.domain.Hashtag;
+import com.mabcci.domain.hashtag.domain.HashtagRepository;
+import com.mabcci.domain.hashtag.dto.HashtagRegisterRequest;
+import com.mabcci.domain.hashtag.dto.HashtagRegisterResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class HashtagService {
+
+    private final HashtagRepository hashtagRepository;
+
+    public HashtagService(final HashtagRepository hashtagRepository) {
+        this.hashtagRepository = hashtagRepository;
+    }
+
+    @Transactional
+    public HashtagRegisterResponse saveHashtags(final HashtagRegisterRequest hashtagRegisterRequest) {
+        final List<String> hashtagNames = hashtagRegisterRequest.getNames();
+        return new HashtagRegisterResponse(hashtagNames.stream()
+                .map(name -> hashtagRepository.findByName(name)
+                        .orElse(hashtagRepository.save(Hashtag.builder()
+                                .name(name)
+                                .build())))
+                .collect(Collectors.toList()));
+    }
+}
