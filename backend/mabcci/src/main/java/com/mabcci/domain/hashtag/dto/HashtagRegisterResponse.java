6b76821e4bc674a8da7f4472ba8454b9b839fdf4
김성규 <kskyu610@gmail.com>
2021-08-04 16:34:33 +0900

    [S05P13C107-84] [BE-kskyu610] feat: HahstagRegisterResponse 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/hashtag/dto/HashtagRegisterResponse.java
@@ -0,0 +1,20 @@
+package com.mabcci.domain.hashtag.dto;
+
+import com.mabcci.domain.hashtag.domain.Hashtag;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public final class HashtagRegisterResponse {
+
+    @NotEmpty
+    private List<Hashtag> hashtags;
+
+    private HashtagRegisterResponse() {
+    }
+
+    public HashtagRegisterResponse(final List<Hashtag> hashtags) {
+        this.hashtags = hashtags;
+    }
+}
