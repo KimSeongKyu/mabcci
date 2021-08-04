2b02c2191db2ffbd203b9ca968247b9d9a7ca3e6
김성규 <kskyu610@gmail.com>
2021-08-04 17:12:42 +0900

    [S05P13C107-84] [BE-kskyu610] test: HahstagRepository findByName 기능 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/hashtag/domain/HashtagRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/domain/HashtagRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.mabcci.domain.hashtag.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -15,6 +19,18 @@ class HashtagRepositoryTest {
 
     @Autowired
     private HashtagRepository hashtagRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    private Hashtag hashtag;
+
+    @BeforeEach
+    void setUp() {
+        hashtag = Hashtag.builder()
+                .name("해시태그")
+                .build();
+    }
 
     @DisplayName("HashtagRepository 구현체 존재 여부 테스트")
     @Test
@@ -34,5 +50,17 @@ class HashtagRepositoryTest {
         final Hashtag savedHashtag = hashtagRepository.save(hashtag);
 
         assertThat(savedHashtag.id()).isEqualTo(hashtag.id());
+    }
+
+    @DisplayName("HashtagRepository findByName 기능 테스트")
+    @Test
+    void find_by_name_test() {
+        testEntityManager.persist(hashtag);
+        final Optional<Hashtag> foundHashtag = hashtagRepository.findByName(hashtag.name());
+
+        assertAll(
+                () -> assertThat(foundHashtag).isNotEmpty(),
+                () -> assertThat(foundHashtag.get().id()).isEqualTo(hashtag.id())
+        );
     }
 }
