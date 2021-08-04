657af4fc124dc137c0970af9f6b07cd4668ba591
김성규 <kskyu610@gmail.com>
2021-08-04 23:16:25 +0900

    [S05P13C107-84] [BE-kskyu610] test: OotdHashtagRegisterRequest 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdhashtag/dto/OotdHashtagRegisterRequestTest.java
@@ -0,0 +1,35 @@
+package com.mabcci.domain.ootdhashtag.dto;
+
+import com.mabcci.domain.hashtag.domain.Hashtag;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mabcci.domain.hashtag.domain.HashtagTest.HASHTAG;
+import static com.mabcci.domain.ootd.domain.OotdTest.OOTD;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class OotdHashtagRegisterRequestTest {
+
+    private OotdHashtagRegisterRequest ootdHashtagRegisterRequest;
+    private List<Hashtag> hashtags;
+
+    @BeforeEach
+    void setUp() {
+        hashtags = new ArrayList<>(List.of(HASHTAG, HASHTAG));
+        ootdHashtagRegisterRequest = new OotdHashtagRegisterRequest(OOTD, hashtags);
+    }
+
+    @DisplayName("OotdHashtagRegisterRequest 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(ootdHashtagRegisterRequest).isNotNull(),
+                () -> assertThat(ootdHashtagRegisterRequest).isExactlyInstanceOf(OotdHashtagRegisterRequest.class)
+        );
+    }
+}
