24615383c7803fca9da57188d74718089d5d037f
김우재 <kwj1270@naver.com>
2021-08-05 10:59:43 +0900

    [S05P13C107-83] [BE-kwj1270] feat: FollowController 인스턴스 팔로우 등록 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/follow/ui/FollowController.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/follow/ui/FollowController.java
@@ -1,6 +1,10 @@
 package com.mabcci.domain.follow.ui;
 
 import com.mabcci.domain.follow.application.FollowService;
+import com.mabcci.domain.follow.dto.FollowSaveRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +14,12 @@ public class FollowController {
 
     public FollowController(final FollowService followService) {
         this.followService = followService;
+    }
+
+    @PostMapping("/api/follow")
+    public ResponseEntity<?> save(@RequestBody FollowSaveRequest request) {
+        final Long followId = followService.save(request.following(), request.follower());
+        return ResponseEntity.ok().body(followId);
     }
 
 }
