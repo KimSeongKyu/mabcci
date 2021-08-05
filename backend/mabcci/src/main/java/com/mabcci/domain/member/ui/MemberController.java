eeeb83cf7a41f91ed33b65d666ec3a1ac5bb4bdf
김우재 <kwj1270@naver.com>
2021-08-05 10:55:09 +0900

    [S05P13C107-83] [BE-kwj1270] test :FollowController 인스턴스 팔로우 등록 기능 테스트 작성

--- backend/mabcci/src/main/java/com/mabcci/domain/member/ui/MemberController.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/member/ui/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:*")
+@CrossOrigin(originPatterns = "http://localhost:*")
 @RestController
 public class MemberController {
 
--- backend/mabcci/src/test/java/com/mabcci/domain/follow/ui/FollowControllerTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/follow/ui/FollowControllerTest.java
@@ -1,21 +1,42 @@
 package com.mabcci.domain.follow.ui;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mabcci.domain.follow.application.FollowService;
+import com.mabcci.domain.follow.dto.FollowSaveRequest;
+import com.mabcci.global.common.Nickname;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class FollowControllerTest {
 
     @Mock private FollowService followService;
     @InjectMocks private FollowController followController;
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(followController).build();
+        objectMapper = new ObjectMapper();
+    }
 
     @DisplayName("FollowController 인스턴스 생성 여부 테스트")
     @Test
@@ -27,4 +48,24 @@ class FollowControllerTest {
                 () -> assertThat(followController).isExactlyInstanceOf(FollowController.class)
         );
     }
+
+    @DisplayName("FollowController 인스턴스 팔로우 등록 기능 테스트")
+    @Test
+    void save_test() throws Exception {
+        given(followService.save(any(), any())).willReturn(1L);
+        final Nickname following = Nickname.of("following");
+        final Nickname follower = Nickname.of("follower");
+        final FollowSaveRequest followSaveRequest = new FollowSaveRequest(following, follower);
+        final String followSaveRequestString = objectMapper.writeValueAsString(followSaveRequest);
+
+        mockMvc.perform(post("/api/follow")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(followSaveRequestString))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+    }
+
+
+
 }
