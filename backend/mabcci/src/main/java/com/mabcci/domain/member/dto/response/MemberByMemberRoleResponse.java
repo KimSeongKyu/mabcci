e739cc28ddaf99e553807af30b8662de46094c9c
김우재 <kwj1270@naver.com>
2021-08-05 14:59:01 +0900

    [S05P13C107-53] [BE-kwj1270] feat: MemberByMemberRoleResponse 인스턴스 정적 팩토리 메서드를 이용한 생성 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/member/dto/response/MemberByMemberRoleResponse.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/member/dto/response/MemberByMemberRoleResponse.java
@@ -2,7 +2,29 @@ package com.mabcci.domain.member.dto.response;
 
 import com.mabcci.global.common.Nickname;
 
-public class MemberByMemberRoleResponse {
+import javax.validation.Valid;
+import java.util.Set;
+
+public final class MemberByMemberRoleResponse {
 
     private Nickname nickname;
+    private String picture;
+    private Set<String> categories;
+
+    public static final MemberByMemberRoleResponse createMemberByMemberRoleResponse(@Valid final Nickname nickname,
+                                                                              final String picture,
+                                                                              final Set<String> categories) {
+        return new MemberByMemberRoleResponse(nickname, picture, categories);
+    }
+
+    MemberByMemberRoleResponse() {
+    }
+
+    private MemberByMemberRoleResponse(@Valid final Nickname nickname,
+                                       final String picture,
+                                       final Set<String> categories) {
+        this.nickname = nickname;
+        this.picture = picture;
+        this.categories = categories;
+    }
 }
