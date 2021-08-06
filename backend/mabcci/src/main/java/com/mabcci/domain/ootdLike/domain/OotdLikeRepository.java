a0c53163326d10257a4f6ddb060fe819a055cad3
김성규 <kskyu610@gmail.com>
2021-08-06 11:43:12 +0900

    [S05P13C107-90] [BE-kskyu610] feat: OotdLikeRepository ootd의 좋아요 수를 반환하는 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/ootdLike/domain/OotdLikeRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdLike/domain/OotdLikeRepository.java
@@ -1,6 +1,8 @@
 package com.mabcci.domain.ootdLike.domain;
 
+import com.mabcci.domain.ootd.domain.Ootd;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OotdLikeRepository extends JpaRepository<OotdLike, Long> {
+    Long countByOotd(Ootd ootd);
 }
