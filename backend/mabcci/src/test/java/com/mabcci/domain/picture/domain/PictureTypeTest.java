219bf13112425f8f3d59c03dd3bfcd20ce59c624
김성규 <kskyu610@gmail.com>
2021-08-06 10:16:07 +0900

    [S05P13C107-90] [BE-kskyu610] test: PictureType 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/picture/domain/PictureTypeTest.java
@@ -0,0 +1,22 @@
+package com.mabcci.domain.picture.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PictureTypeTest {
+
+    @DisplayName("PictureType 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        Arrays.stream(PictureType.values())
+                .forEach(pictureType -> assertAll(
+                        () -> assertThat(pictureType).isNotNull(),
+                        () -> assertThat(pictureType).isExactlyInstanceOf(PictureType.class))
+                );
+    }
+}
