3232e2b6671a1133503ffb522acfdd367c402be7
김성규 <kskyu610@gmail.com>
2021-08-08 21:18:15 +0900

    [S05P13C107-90] [BE-kskyu610] fix: OotdService Ootd 리스트 조회 코드 오류 수정

--- backend/mabcci/src/test/java/com/mabcci/domain/ootd/application/OotdServiceTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootd/application/OotdServiceTest.java
@@ -1,5 +1,6 @@
 package com.mabcci.domain.ootd.application;
 
+import com.mabcci.domain.hashtag.domain.Hashtag;
 import com.mabcci.domain.member.domain.Gender;
 import com.mabcci.domain.member.domain.Member;
 import com.mabcci.domain.member.domain.MemberRepository;
@@ -9,6 +10,10 @@ import com.mabcci.domain.ootd.domain.OotdRepository;
 import com.mabcci.domain.ootd.dto.OotdListResponse;
 import com.mabcci.domain.ootd.dto.OotdSaveRequest;
 import com.mabcci.domain.ootdLike.domain.OotdLikeRepository;
+import com.mabcci.domain.ootdhashtag.domain.OotdHashtag;
+import com.mabcci.domain.ootdhashtag.domain.OotdHashtagRepository;
+import com.mabcci.domain.ootdpicture.domain.OotdPicture;
+import com.mabcci.domain.ootdpicture.domain.OotdPictureRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +21,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
@@ -42,11 +49,23 @@ class OotdServiceTest {
     @InjectMocks private OotdService ootdService;
 
     @Mock
+    private OotdPictureRepository ootdPictureRepository;
+
+    @Mock
+    private OotdHashtagRepository ootdHashtagRepository;
+
+    @Mock
     private OotdLikeRepository ootdLikeRepository;
 
     private Member member;
     private Ootd ootd;
     private List<Ootd> ootds;
+    private OotdPicture ootdPicture;
+    private Hashtag firstHashtag;
+    private Hashtag secondHashtag;
+    private OotdHashtag firstOotdHashtag;
+    private OotdHashtag secondOotdHashtag;
+    private List<OotdHashtag> ootdHashtags;
 
     @BeforeEach
     void setUp() {
@@ -90,6 +109,25 @@ class OotdServiceTest {
                         .views(0L)
                         .build()
         ));
+        ootdPicture = OotdPicture.builder()
+                .url("testUrl")
+                .fileName("testFileName")
+                .build();
+        firstHashtag = Hashtag.builder()
+                .name("해시태그1")
+                .build();
+        secondHashtag = Hashtag.builder()
+                .name("해시태그2")
+                .build();
+        firstOotdHashtag = OotdHashtag.builder()
+                .ootd(ootd)
+                .hashtag(firstHashtag)
+                .build();
+        secondOotdHashtag = OotdHashtag.builder()
+                .ootd(ootd)
+                .hashtag(firstHashtag)
+                .build();
+        ootdHashtags = new ArrayList<>(List.of(firstOotdHashtag, secondOotdHashtag));
     }
 
     @DisplayName("OotdService 인스턴스 ootd 저장 테스트")
@@ -107,17 +145,22 @@ class OotdServiceTest {
     @DisplayName("OotdService 인스턴스 필터링된 ootd 리스트 조회 테스트")
     @Test
     void find_filtered_ootd_list_test() {
-        doReturn(ootds).when(ootdRepository).findAll((Pageable) any());
+        final Page<Ootd> ootdPages = new PageImpl<>(ootds);
+        doReturn(ootdPages).when(ootdRepository).findAll((Pageable) any());
+        doReturn(Optional.of(ootdPicture)).when(ootdPictureRepository).findFirstByOotd(any());
+        doReturn(ootdHashtags).when(ootdHashtagRepository).findByOotd(any());
         doReturn(1L).when(ootdLikeRepository).countByOotd(any());
 
         final OotdListResponse ootdListResponse = ootdService.findFilteredOotdList("all", PageRequest.of(1, 20));
 
         verify(ootdRepository, times(1)).findAll((Pageable) any());
-        verify(ootdLikeRepository, times(1)).countByOotd(any());
+        verify(ootdPictureRepository, times(2)).findFirstByOotd(any());
+        verify(ootdHashtagRepository, times(2)).findByOotd(any());
+        verify(ootdLikeRepository, times(2)).countByOotd(any());
 
         assertAll(
-                () -> assertThat(ootdListResponse.getOotdResponses()).isEqualTo(ootds),
-                () -> assertThat(ootdListResponse.getTotalPages()).isEqualTo(1L)
+                () -> assertThat(ootdListResponse.getOotdResponses().size()).isEqualTo(2),
+                () -> assertThat(ootdListResponse.getTotalPages()).isEqualTo(1)
         );
     }
 }
