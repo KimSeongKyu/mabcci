a3e355e547be771dfef101b5459672714d4241da
김성규 <kskyu610@gmail.com>
2021-08-08 16:51:56 +0900

    [S05P13C107-90] [BE-kskyu610] test: OotdListResponse 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootd/dto/OotdListResponseTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootd/dto/OotdListResponseTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -41,6 +45,23 @@ class OotdListResponseTest {
         assertAll(
                 () -> assertThat(ootdListResponse.getOotdResponses()).isEqualTo(ootdResponses),
                 () -> assertThat(ootdListResponse.getTotalPages()).isEqualTo(1L)
+        );
+    }
+
+    @DisplayName("OotdListResponse 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdListResponse invalidResponse = new OotdListResponse(new ArrayList<>(), -1L);
+
+        final Set<ConstraintViolation<OotdListResponse>> invalidPropertiesOfValidResponse =
+                validator.validate(ootdListResponse);
+        final Set<ConstraintViolation<OotdListResponse>> invalidPropertiesOfInvalidResponse =
+                validator.validate(invalidResponse);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidResponse.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidResponse.size()).isEqualTo(2)
         );
     }
 }
