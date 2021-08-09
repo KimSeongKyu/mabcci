b779146310249d2dc418d275467fe9c2e4def20f
김성규 <kskyu610@gmail.com>
2021-08-09 15:08:45 +0900

    [S05P13C107-91] [BE-kskyu610] test: OotdUpdateRequest 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootd/dto/OotdUpdateRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootd/dto/OotdUpdateRequestTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -39,6 +43,23 @@ public class OotdUpdateRequestTest {
                 () -> assertThat(ootdUpdateRequest.getShoes()).isEqualTo("신발"),
                 () -> assertThat(ootdUpdateRequest.getAccessory()).isEqualTo("악세사리"),
                 () -> assertThat(ootdUpdateRequest.getHashtags()).contains(new String[]{"해시태그1", "해시태그2"})
+        );
+    }
+
+    @DisplayName("OotdUpdateRequest 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdUpdateRequest invalidRequest = new OotdUpdateRequest(null, null, null, null, null, null);
+
+        final Set<ConstraintViolation<OotdUpdateRequest>> invalidPropertiesOfValidRequest =
+                validator.validate(ootdUpdateRequest);
+        final Set<ConstraintViolation<OotdUpdateRequest>> invalidPropertiesOfInvalidRequest =
+                validator.validate(invalidRequest);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidRequest.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidRequest.size()).isEqualTo(1)
         );
     }
 }
