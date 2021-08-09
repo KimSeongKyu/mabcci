c1d5c65e632072b79355d85e1e9ad26eb936f29b
김성규 <kskyu610@gmail.com>
2021-08-09 15:02:22 +0900

    [S05P13C107-91] [BE-kskyu610] feat: OotdUpdateRequest 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootd/dto/OotdUpdateRequest.java
@@ -0,0 +1,29 @@
+package com.mabcci.domain.ootd.dto;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+public final class OotdUpdateRequest {
+
+    @NotEmpty
+    private String content;
+
+    private String top;
+    private String bottom;
+    private String shoes;
+    private String accessory;
+    private List<String> hashtags;
+
+    private OotdUpdateRequest() {
+    }
+
+    public OotdUpdateRequest(final String content, final String top, final String bottom, final String shoes,
+                             final String accessory, final List<String> hashtags) {
+        this.content = content;
+        this.top = top;
+        this.bottom = bottom;
+        this.shoes = shoes;
+        this.accessory = accessory;
+        this.hashtags = hashtags;
+    }
+}
