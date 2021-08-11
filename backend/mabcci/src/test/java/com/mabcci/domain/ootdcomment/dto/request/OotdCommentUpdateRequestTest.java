b19b49c91995d259acb78571496fcbf1c953e3dc
김성규 <kskyu610@gmail.com>
2021-08-11 23:59:51 +0900

    [S05P13C107-100] [BE-kskyu610] test: OotdCommentUpdateRequest 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/request/OotdCommentUpdateRequestTest.java
@@ -0,0 +1,27 @@
+package com.mabcci.domain.ootdcomment.dto.request;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class OotdCommentUpdateRequestTest {
+
+    private OotdCommentUpdateRequest ootdCommentUpdateRequest;
+
+    @BeforeEach
+    void setUp() {
+        ootdCommentUpdateRequest = new OotdCommentUpdateRequest("수정된 내용");
+    }
+
+    @DisplayName("OotdCommentUpdateRequest 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(ootdCommentUpdateRequest).isNotNull(),
+                () -> assertThat(ootdCommentUpdateRequest).isExactlyInstanceOf(OotdCommentUpdateRequest.class)
+        );
+    }
+}
