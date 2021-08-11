e8304659c09facfcf4ba597f4f95ab45358d9a11
김성규 <kskyu610@gmail.com>
2021-08-12 00:03:04 +0900

    [S05P13C107-100] [BE-kskyu610] feat: OotdCommentUpdateRequest 인스턴스 getter 메서드 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/dto/request/OotdCommentUpdateRequest.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/dto/request/OotdCommentUpdateRequest.java
@@ -1,10 +1,17 @@
 package com.mabcci.domain.ootdcomment.dto.request;
 
+import javax.validation.constraints.NotEmpty;
+
 public final class OotdCommentUpdateRequest {
 
+    @NotEmpty
     private String content;
 
     public OotdCommentUpdateRequest(final String content) {
         this.content = content;
+    }
+
+    public final String getContent() {
+        return content;
     }
 }
