c0566a31810687804e05e6f3b16ed3b572d63078
김성규 <kskyu610@gmail.com>
2021-08-12 00:31:57 +0900

    [S05P13C107-100] [BE-kskyu610] test: OotdCommentController 인스턴스 ootd 댓글 수정 API 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/ui/OotdCommentControllerTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/ui/OotdCommentControllerTest.java
@@ -2,7 +2,9 @@ package com.mabcci.domain.ootdcomment.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mabcci.domain.ootdcomment.application.OotdCommentSaveService;
+import com.mabcci.domain.ootdcomment.application.OotdCommentUpdateService;
 import com.mabcci.domain.ootdcomment.dto.request.OotdCommentSaveRequest;
+import com.mabcci.domain.ootdcomment.dto.request.OotdCommentUpdateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import static com.mabcci.global.common.NicknameTest.NICKNAME;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(OotdCommentController.class)
@@ -23,6 +26,7 @@ class OotdCommentControllerTest {
     @Autowired private MockMvc mockMvc;
     @Autowired private ObjectMapper objectMapper;
     @MockBean private OotdCommentSaveService ootdCommentSaveService;
+    @MockBean private OotdCommentUpdateService ootdCommentUpdateService;
 
     @DisplayName("OotdCommentController 인스턴스 ootd 댓글 저장 API 테스트")
     @Test
@@ -35,7 +39,21 @@ class OotdCommentControllerTest {
         mockMvc.perform(post("/api/ootd/comments")
                 .content(objectMapper.writeValueAsString(ootdCommentSaveRequest))
                 .contentType(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @DisplayName("OotdCommentController 인스턴스 ootd 댓글 수정 API 테스트")
+    @Test
+    void update_ootd_comment_api_test() throws Exception {
+        final OotdCommentUpdateRequest ootdCommentUpdateRequest = new OotdCommentUpdateRequest("수정된 내용");
+
+        doNothing().when(ootdCommentUpdateService).updateOotdComment(any(), any());
+
+        mockMvc.perform(put("/api/ootd/comments/" + 1)
+                .content(objectMapper.writeValueAsString(ootdCommentUpdateRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
     }
 }
