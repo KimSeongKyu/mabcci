f57909ee2bedc3b8a6f2b8f8725c6e742e028a89
김성규 <kskyu610@gmail.com>
2021-08-11 21:33:07 +0900

    [S05P13C107-99] [BE-kskyu610] feat: OotdCommentRepository 인터페이스 작성

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/domain/OotdCommentRepository.java
@@ -0,0 +1,6 @@
+package com.mabcci.domain.ootdcomment.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OotdCommentRepository extends JpaRepository<OotdComment, Long> {
+}
