3fb8d615eabe472b32be49976c7eb115ce81428d
김성규 <kskyu610@gmail.com>
2021-08-11 22:33:40 +0900

    [S05P13C107-99] [BE-kskyu610] test: OotdCommentSaveRequest 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/request/OotdCommentSaveRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/request/OotdCommentSaveRequestTest.java
@@ -1,8 +1,15 @@
 package com.mabcci.domain.ootdcomment.dto.request;
 
+import com.mabcci.global.common.Nickname;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import java.util.Set;
 
 import static com.mabcci.global.common.NicknameTest.NICKNAME;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,6 +41,23 @@ class OotdCommentSaveRequestTest {
                 () -> assertThat(ootdCommentSaveRequest.getNickname()).isEqualTo(NICKNAME),
                 () -> assertThat(ootdCommentSaveRequest.getParentCommentId()).isNull(),
                 () -> assertThat(ootdCommentSaveRequest.getContent()).isEqualTo("내용")
+        );
+    }
+
+    @DisplayName("OotdCommentRequest 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdCommentSaveRequest invalidRequest = new OotdCommentSaveRequest(0L, Nickname.of(""), 0L, "");
+
+        final Set<ConstraintViolation<OotdCommentSaveRequest>> invalidPropertiesOfValidRequest =
+                validator.validate(ootdCommentSaveRequest);
+        final Set<ConstraintViolation<OotdCommentSaveRequest>> invalidPropertiesOfInvalidRequest =
+                validator.validate(invalidRequest);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidRequest.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidRequest.size()).isEqualTo(4)
         );
     }
 }
