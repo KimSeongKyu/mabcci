252a02d28877529c824314e2a3386816eaec55f8
김성규 <kskyu610@gmail.com>
2021-08-12 14:12:54 +0900

    [S05P13C107-103] [BE-kskyu610] test: OotdCommentResponse 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
@@ -0,0 +1,30 @@
+package com.mabcci.domain.ootdcomment.dto.response;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static com.mabcci.global.common.NicknameTest.NICKNAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class OotdCommentResponseTest {
+
+    private OotdCommentResponse ootdCommentResponse;
+
+    @BeforeEach
+    void setUp() {
+        ootdCommentResponse = new OotdCommentResponse("testUrl", NICKNAME, LocalDateTime.now(), LocalDateTime.now(), "내용");
+    }
+
+    @DisplayName("OotdCommentResponse 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(ootdCommentResponse).isNotNull(),
+                () -> assertThat(ootdCommentResponse).isExactlyInstanceOf()
+        );
+    }
+}
