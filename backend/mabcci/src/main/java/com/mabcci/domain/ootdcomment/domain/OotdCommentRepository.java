3be0b649985f28a966cf3888c60750d0fb06c520
김성규 <kskyu610@gmail.com>
2021-08-12 13:39:09 +0900

    [S05P13C107-103] [BE-kskyu610 feat: OotdCommentRepository ootd의 댓글 리스트 조회 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/domain/OotdCommentRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/domain/OotdCommentRepository.java
@@ -1,6 +1,10 @@
 package com.mabcci.domain.ootdcomment.domain;
 
+import com.mabcci.domain.ootd.domain.Ootd;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OotdCommentRepository extends JpaRepository<OotdComment, Long> {
+    List<OotdComment> findAllByOotd(Ootd ootd);
 }
