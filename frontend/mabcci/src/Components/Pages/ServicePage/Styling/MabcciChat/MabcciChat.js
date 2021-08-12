57c9aba02c45806fc783c1ec485f2f9bb6a2ca92
meganatc7 <meganatc7@gmail.com>
2021-08-12 10:28:56 +0900

    [S05P13C107-93][FE-linkin2love]nav바 수정

--- frontend/mabcci/src/Components/Pages/ServicePage/Styling/MabcciChat/MabcciChat.js
+++ frontend/mabcci/src/Components/Pages/ServicePage/Styling/MabcciChat/MabcciChat.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import NavCategory from '../../../../../Redux/Actions/NavAction';
 import ChatRoom from './ChatRoom';
 import ChatSide from './ChatSide';
 import './MabcciChat.css';
 
 const MabcciChat = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(NavCategory('chat'));
+  }, []);
   const [chatMenu, setChatMenu] = useState(false);
   return (
     <div className="container chat">
