50869ee774dc7ee8fd04892fc50c27a52b8fbcd3
김성규 <kskyu610@gmail.com>
2021-08-12 14:30:31 +0900

    [S05P13C107-103] [BE-kskyu610] test: OotdCommentResponse 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
@@ -1,10 +1,15 @@
 package com.mabcci.domain.ootdcomment.dto.response;
 
+import com.mabcci.global.common.Nickname;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import static com.mabcci.global.common.NicknameTest.NICKNAME;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,6 +43,24 @@ public class OotdCommentResponseTest {
                 () -> assertThat(ootdCommentResponse.getCreatedDate()).isEqualTo(now),
                 () -> assertThat(ootdCommentResponse.getModifiedDate()).isEqualTo(now),
                 () -> assertThat(ootdCommentResponse.getContent()).isEqualTo("내용")
+        );
+    }
+
+    @DisplayName("OotdCommentResponse 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final OotdCommentResponse invalidResponse =
+                new OotdCommentResponse("", Nickname.of(null), LocalDateTime.MAX, LocalDateTime.MAX, "");
+
+        final Set<ConstraintViolation<OotdCommentResponse>> invalidPropertiesOfValidResponse =
+                validator.validate(ootdCommentResponse);
+        final Set<ConstraintViolation<OotdCommentResponse>> invalidPropertiesOfInvalidResponse =
+                validator.validate(invalidResponse);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidResponse.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidResponse.size()).isEqualTo(5)
         );
     }
 }
