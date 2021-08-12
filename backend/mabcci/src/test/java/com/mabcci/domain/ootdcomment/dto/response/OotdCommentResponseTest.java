03582c16a950b9e1895e7c2032cfd785db69f3bb
김성규 <kskyu610@gmail.com>
2021-08-12 23:58:24 +0900

    [S05P13C107-103] [BE-kskyu610] test: OotdCommentResponse 클래스 스태틱 팩토리 메서드를 이용한 생성 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
@@ -1,5 +1,10 @@
 package com.mabcci.domain.ootdcomment.dto.response;
 
+import com.mabcci.domain.member.domain.Gender;
+import com.mabcci.domain.member.domain.Member;
+import com.mabcci.domain.member.domain.MemberRole;
+import com.mabcci.domain.ootd.domain.Ootd;
+import com.mabcci.domain.ootdcomment.domain.OotdComment;
 import com.mabcci.global.common.Nickname;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,9 +15,15 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Set;
 
+import static com.mabcci.domain.member.domain.MemberTest.DESCRIPTION;
+import static com.mabcci.domain.member.domain.MemberTest.PICTURE;
+import static com.mabcci.global.common.EmailTest.EMAIL;
 import static com.mabcci.global.common.NicknameTest.NICKNAME;
+import static com.mabcci.global.common.PasswordTest.PASSWORD;
+import static com.mabcci.global.common.PhoneTest.PHONE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -20,15 +31,55 @@ public class OotdCommentResponseTest {
 
     private OotdCommentResponse ootdCommentResponse;
     private LocalDateTime now = LocalDateTime.now();
+    private Member member;
+    private Ootd ootd;
+    private OotdComment ootdComment;
 
     @BeforeEach
     void setUp() {
         ootdCommentResponse = new OotdCommentResponse("testUrl", NICKNAME, now, now, "내용", new ArrayList<>());
+        member = Member.Builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
+                .phone(PHONE)
+                .gender(Gender.MAN)
+                .description(DESCRIPTION)
+                .picture(PICTURE)
+                .memberRole(MemberRole.USER)
+                .build();
+        ootd = Ootd.builder()
+                .member(member)
+                .content("content")
+                .top("top")
+                .bottom("bottom")
+                .shoes("shoes")
+                .accessory("accessory")
+                .views(0L)
+                .build();
+        ootdComment = OotdComment.builder()
+                .member(member)
+                .ootd(ootd)
+                .parentComment(null)
+                .content("내용")
+                .build();
     }
 
     @DisplayName("OotdCommentResponse 인스턴스 생성 여부 테스트")
     @Test
     void initialize() {
+        assertAll(
+                () -> assertThat(ootdCommentResponse).isNotNull(),
+                () -> assertThat(ootdCommentResponse).isExactlyInstanceOf(OotdCommentResponse.class)
+        );
+    }
+
+    @DisplayName("OotdCommentResponse 클래스 스태틱 팩토리 메서드를 이용한 생성 테스트")
+    @Test
+    void static_factory_method_constructor_test() {
+        final OotdCommentResponse ootdCommentResponse =
+                OotdCommentResponse.ofOotdCommentWithChildren(ootdComment, Collections.emptyList());
+
         assertAll(
                 () -> assertThat(ootdCommentResponse).isNotNull(),
                 () -> assertThat(ootdCommentResponse).isExactlyInstanceOf(OotdCommentResponse.class)
