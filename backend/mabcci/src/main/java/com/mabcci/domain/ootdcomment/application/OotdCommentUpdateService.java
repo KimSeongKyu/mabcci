2ea2805225ff7a515b9500137cfde8f89a7146f8
김성규 <kskyu610@gmail.com>
2021-08-12 11:43:58 +0900

    [S05P13C107-98] [BE-kskyu610 feat: OotdCommentUpdateService 인스턴스 ootd 댓글 수정자가 댓글 작성자인지 검증 로직 추가

--- backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/application/OotdCommentUpdateService.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdcomment/application/OotdCommentUpdateService.java
@@ -3,8 +3,11 @@ package com.mabcci.domain.ootdcomment.application;
 import com.mabcci.domain.ootdcomment.domain.OotdComment;
 import com.mabcci.domain.ootdcomment.domain.OotdCommentRepository;
 import com.mabcci.domain.ootdcomment.dto.request.OotdCommentUpdateRequest;
+import com.mabcci.global.common.Nickname;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Service
 public class OotdCommentUpdateService {
@@ -18,11 +21,20 @@ public class OotdCommentUpdateService {
     @Transactional
     public void updateOotdComment(final Long id, final OotdCommentUpdateRequest ootdCommentUpdateRequest) {
         final OotdComment ootdComment = getOotdCommentById(id);
+        validateMemberIsOotdCommentWriter(ootdComment, ootdCommentUpdateRequest.getNickname());
         ootdComment.update(ootdCommentUpdateRequest.getContent());
     }
 
     private OotdComment getOotdCommentById(final Long id) {
         return ootdCommentRepository.findById(id)
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private void validateMemberIsOotdCommentWriter(final OotdComment ootdComment, final Nickname nickname) {
+        if(!ootdComment.member()
+                .nickname()
+                .equals(nickname)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
