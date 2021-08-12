765ae70189cad1b1a65a4f5a1df71e27973f0e58
김성규 <kskyu610@gmail.com>
2021-08-12 14:25:36 +0900

    [S05P13C107-103] [BE-kskyu610] test: OotdCommentResponse 인스턴스 getter 메서드들 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/ootdcomment/dto/response/OotdCommentResponseTest.java
@@ -13,10 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 public class OotdCommentResponseTest {
 
     private OotdCommentResponse ootdCommentResponse;
+    private LocalDateTime now = LocalDateTime.now();
 
     @BeforeEach
     void setUp() {
-        ootdCommentResponse = new OotdCommentResponse("testUrl", NICKNAME, LocalDateTime.now(), LocalDateTime.now(), "내용");
+        ootdCommentResponse = new OotdCommentResponse("testUrl", NICKNAME, now, now, "내용");
     }
 
     @DisplayName("OotdCommentResponse 인스턴스 생성 여부 테스트")
@@ -25,6 +26,18 @@ public class OotdCommentResponseTest {
         assertAll(
                 () -> assertThat(ootdCommentResponse).isNotNull(),
                 () -> assertThat(ootdCommentResponse).isExactlyInstanceOf(OotdCommentResponse.class)
+        );
+    }
+
+    @DisplayName("OotdCommentResponse 인스턴스 getter 메서드들 테스트")
+    @Test
+    void getter_test() {
+        assertAll(
+                () -> assertThat(ootdCommentResponse.getMemberPicture()).isEqualTo("testUrl"),
+                () -> assertThat(ootdCommentResponse.getMemberNickname()).isEqualTo(NICKNAME),
+                () -> assertThat(ootdCommentResponse.getCreatedDate()).isEqualTo(now),
+                () -> assertThat(ootdCommentResponse.getModifiedDate()).isEqualTo(now),
+                () -> assertThat(ootdCommentResponse.getContent()).isEqualTo("내용")
         );
     }
 }
