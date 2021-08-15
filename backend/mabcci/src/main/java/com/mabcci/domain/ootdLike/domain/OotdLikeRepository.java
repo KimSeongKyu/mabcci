78976817c9216ed07567bae187494cab95d9634d
김성규 <kskyu610@gmail.com>
2021-08-15 11:24:06 +0900

    [S05P13C107-104] [BE-kskyu610] feat: OotdLikeRepository 인스턴스 ootd와 닉네임으로 ootd 좋아요 조회 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/ootdLike/domain/OotdLikeRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdLike/domain/OotdLikeRepository.java
@@ -1,6 +1,18 @@
 package com.mabcci.domain.ootdLike.domain;
 
+import com.mabcci.domain.ootd.domain.Ootd;
+import com.mabcci.global.common.Nickname;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OotdLikeRepository extends JpaRepository<OotdLike, Long> {
+
+    @Query("SELECT o_l " +
+            "FROM OotdLike o_l " +
+            "JOIN FETCH o_l.member m " +
+            "WHERE o_l.ootd = :ootd AND m.nickname = :nickname")
+    Optional<OotdLike> findByOotdAndNickname(@Param("ootd") Ootd ootd, @Param("nickname") Nickname nickname);
 }
