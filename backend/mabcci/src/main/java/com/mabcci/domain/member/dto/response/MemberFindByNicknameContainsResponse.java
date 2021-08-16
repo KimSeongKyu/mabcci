0295222b7b36537ad8e81e399cb599ed9ab50598
김성규 <kskyu610@gmail.com>
2021-08-16 17:26:50 +0900

    [S05P13C107-105] [BE-kskyu610] feat: MemberFindByNicknameContainsResponse 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/member/dto/response/MemberFindByNicknameContainsResponse.java
@@ -0,0 +1,21 @@
+package com.mabcci.domain.member.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mabcci.global.common.Nickname;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public final class MemberFindByNicknameContainsResponse {
+
+    @Valid @NotNull @JsonProperty("nickname")
+    private Nickname nickname;
+
+    @JsonProperty("picture")
+    private String picture;
+
+    public MemberFindByNicknameContainsResponse(final Nickname nickname, final String picture) {
+        this.nickname = nickname;
+        this.picture = picture;
+    }
+}
