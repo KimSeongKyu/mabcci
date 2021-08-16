645ad008354e0011b428f42f3c916ffcf5e01fa1
김성규 <kskyu610@gmail.com>
2021-08-16 12:04:01 +0900

    [S05P13C107-104] [BE-kskyu610] feat: OotdLikeController 인스턴스 좋아요 생성 및 수정 API 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/ootdLike/ui/OotdLikeController.java
@@ -0,0 +1,31 @@
+package com.mabcci.domain.ootdLike.ui;
+
+import com.mabcci.domain.ootdLike.application.OotdLikeSaveAndUpdateService;
+import com.mabcci.global.common.Nickname;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@CrossOrigin(originPatterns = "http://localhost:*")
+@RestController
+public class OotdLikeController {
+
+    private final OotdLikeSaveAndUpdateService ootdLikeSaveAndUpdateService;
+
+    public OotdLikeController(final OotdLikeSaveAndUpdateService ootdLikeSaveAndUpdateService) {
+        this.ootdLikeSaveAndUpdateService = ootdLikeSaveAndUpdateService;
+    }
+
+    @PostMapping("/api/ootd/{id}/like/{nickname}")
+    public ResponseEntity saveOrUpdateOotdLike(@Positive @PathVariable("id") final Long id,
+                                               @Valid @NotNull @PathVariable("nickname") final Nickname nickname) {
+        ootdLikeSaveAndUpdateService.saveOrUpdateOotdLike(id, nickname);
+        return ResponseEntity.noContent().build();
+    }
+}
