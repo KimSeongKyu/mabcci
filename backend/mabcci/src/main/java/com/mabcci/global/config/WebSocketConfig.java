694f93ff663e43744ffa64f8c7b49716de318d7d
김우재 <kwj1270@naver.com>
2021-08-16 14:52:08 +0900

    [S05P13C107-102] [BE-kwj1270] fix: 채팅 메시지 입력 안되는 CORS 에러 해결

--- backend/mabcci/src/main/java/com/mabcci/global/config/WebSocketConfig.java
+++ backend/mabcci/src/main/java/com/mabcci/global/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
