fba992cd833376d495eb737c34406236bd649f61
김성규 <kskyu610@gmail.com>
2021-08-16 17:53:15 +0900

    [S05P13C107-105] [BE-kskyu610] test: HashtagFindByNicknameContainsResponse 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/dto/HashtagFindByNicknameContainsResponseTest.java
@@ -0,0 +1,31 @@
+package com.mabcci.domain.hashtag.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class HashtagFindByNicknameContainsResponseTest {
+
+    private HashtagFindByNicknameContainsResponse hashtagFindByNicknameContainsResponse;
+
+    @BeforeEach
+    void setUp() {
+        hashtagFindByNicknameContainsResponse =
+                new HashtagFindByNicknameContainsResponse(List.of("해시태그1", "해시태그2"));
+    }
+
+    @DisplayName("HashtagFindByNicknameContainsResponse 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(hashtagFindByNicknameContainsResponse).isNotNull(),
+                () -> assertThat(hashtagFindByNicknameContainsResponse)
+                        .isExactlyInstanceOf(HashtagFindByNicknameContainsResponse.class)
+        );
+    }
+}
