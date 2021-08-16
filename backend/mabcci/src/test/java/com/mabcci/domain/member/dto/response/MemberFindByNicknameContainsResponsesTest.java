96b9e40d0b9e46a63a7749d659e9151f4b281e42
김성규 <kskyu610@gmail.com>
2021-08-16 17:44:34 +0900

    [S05P13C107-105] [BE-kskyu610] test: MemberFindByNicknameContainsResponses 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/member/dto/response/MemberFindByNicknameContainsResponsesTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/member/dto/response/MemberFindByNicknameContainsResponsesTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.List;
+import java.util.Set;
 
 import static com.mabcci.global.common.NicknameTest.NICKNAME;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,5 +41,23 @@ class MemberFindByNicknameContainsResponsesTest {
     void getter_test() {
         assertThat(memberFindByNicknameContainsResponses.members())
                 .isEqualTo(List.of(memberFindByNicknameContainsResponse));
+    }
+
+    @DisplayName("MemberFindByNicknameContainsResponses 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final MemberFindByNicknameContainsResponses invalidResponses =
+                new MemberFindByNicknameContainsResponses(null);
+
+        final Set<ConstraintViolation<MemberFindByNicknameContainsResponses>> invalidPropertiesOfValidResponse =
+                validator.validate(memberFindByNicknameContainsResponses);
+        final Set<ConstraintViolation<MemberFindByNicknameContainsResponses>> invalidPropertiesOfInvalidResponse =
+                validator.validate(invalidResponses);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidResponse.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidResponse.size()).isEqualTo(1)
+        );
     }
 }
