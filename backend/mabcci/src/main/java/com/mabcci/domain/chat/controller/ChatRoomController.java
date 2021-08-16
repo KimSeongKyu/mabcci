965a3de407e02867e227365149dfdcc2b6d7f5e3
김우재 <kwj1270@naver.com>
2021-08-16 11:24:01 +0900

    [S05P13C107-102] [BE-kwj1270] feat: Chat 초기 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/chat/controller/ChatRoomController.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/chat/controller/ChatRoomController.java
@@ -1,14 +1,15 @@
 package com.mabcci.domain.chat.controller;
 
-import com.mabcci.domain.chat.domain.ChatRoom;
-import com.mabcci.domain.chat.domain.ChatRoomRepository;
+import com.mabcci.domain.chat.model.ChatRoom;
+import com.mabcci.domain.chat.repo.ChatRoomRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
-@RestController
+@Controller
+@RequestMapping("/chat")
 public class ChatRoomController {
 
     private final ChatRoomRepository chatRoomRepository;
@@ -17,17 +18,31 @@ public class ChatRoomController {
         this.chatRoomRepository = chatRoomRepository;
     }
 
-    @GetMapping("/chat/rooms")
-    public List<ChatRoom> rooms() {
+    @GetMapping("/room")
+    public String rooms(Model model) {
+        return "/chat/room";
+    }
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
         return chatRoomRepository.findAllRoom();
     }
 
     @PostMapping("/room")
+    @ResponseBody
     public ChatRoom createRoom(@RequestParam String name) {
         return chatRoomRepository.createChatRoom(name);
     }
 
-    @GetMapping("room/{roomId}")
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
     public ChatRoom roomInfo(@PathVariable String roomId) {
         return chatRoomRepository.findRoomById(roomId);
     }
