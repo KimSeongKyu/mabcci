cbb9f0505edbbddd4f2751cc637fd12f7b47dbb6
김성규 <kskyu610@gmail.com>
2021-08-16 17:54:31 +0900

    [S05P13C107-105] [BE-kskyu610] feat: HashtagFindByNicknameContainsResponse 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/hashtag/dto/HashtagFindByNicknameContainsResponse.java
@@ -0,0 +1,16 @@
+package com.mabcci.domain.hashtag.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public final class HashtagFindByNicknameContainsResponse {
+
+    @NotNull @JsonProperty("hashtags")
+    private List<String> hashtags;
+
+    public HashtagFindByNicknameContainsResponse(final List<String> hashtags) {
+        this.hashtags = hashtags;
+    }
+}
