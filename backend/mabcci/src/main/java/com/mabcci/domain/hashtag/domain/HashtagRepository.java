ad90652e53a7af50790d29ebe5d66f45359024f8
김성규 <kskyu610@gmail.com>
2021-08-16 17:50:16 +0900

    [S05P13C107-105] [BE-kskyu610] feat: HashtagRepository 이름에 키워드를 포함한 해시태그 리스트 검색 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/hashtag/domain/HashtagRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/hashtag/domain/HashtagRepository.java
@@ -2,8 +2,10 @@ package com.mabcci.domain.hashtag.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
     Optional<Hashtag> findByName(String name);
+    List<Hashtag> findByNameContains(String name);
 }
