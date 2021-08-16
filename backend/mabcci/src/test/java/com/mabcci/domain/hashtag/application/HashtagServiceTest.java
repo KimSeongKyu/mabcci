1dace8f86bd133179b7174a3ab3d5949f08b0f31
김성규 <kskyu610@gmail.com>
2021-08-16 20:47:02 +0900

    [S05P13C107-105] [BE-kskyu610] test: HashtagService 인스턴스 키워드를 이름에 포함한 Hashtag 리스트 검색 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/hashtag/application/HashtagServiceTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/hashtag/application/HashtagServiceTest.java
@@ -2,6 +2,7 @@ package com.mabcci.domain.hashtag.application;
 
 import com.mabcci.domain.hashtag.domain.Hashtag;
 import com.mabcci.domain.hashtag.domain.HashtagRepository;
+import com.mabcci.domain.hashtag.dto.HashtagFindByNicknameContainsResponse;
 import com.mabcci.domain.hashtag.dto.HashtagSaveResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.mabcci.domain.hashtag.dto.HashtagSaveRequestTest.HASHTAG_SAVE_REQUEST;
@@ -32,6 +34,18 @@ public class HashtagServiceTest {
         hashtag = Hashtag.builder()
                 .name("해시태그")
                 .build();
+    }
+
+    @DisplayName("HashtagService 인스턴스 키워드를 이름에 포함한 Hashtag 리스트 검색 테스트")
+    @Test
+    void find_by_name_contains_test() {
+        doReturn(List.of(hashtag)).when(hashtagRepository).findByNameContains(any());
+
+        final HashtagFindByNicknameContainsResponse hashtagFindByNicknameContainsResponse =
+                hashtagService.findByNameContains("해시태그");
+
+        verify(hashtagRepository, times(1)).findByNameContains(any());
+        assertThat(hashtagFindByNicknameContainsResponse.hashtags()).contains("해시태그");
     }
 
     @DisplayName("HashtagService 인스턴스 Hashtag 저장 테스트")
