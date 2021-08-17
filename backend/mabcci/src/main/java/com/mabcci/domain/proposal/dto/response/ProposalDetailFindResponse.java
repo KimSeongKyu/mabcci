f82453863b3508fbe33c04681deb78e71623b17a
김성규 <kskyu610@gmail.com>
2021-08-17 21:37:07 +0900

    [S05P13C107-110] [BE-kskyu610] feat: ProposalDetailFindResponse 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/proposal/dto/response/ProposalDetailFindResponse.java
@@ -0,0 +1,50 @@
+package com.mabcci.domain.proposal.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mabcci.domain.proposal.domain.Proposal;
+import com.mabcci.global.common.Nickname;
+
+public final class ProposalDetailFindResponse {
+
+    @JsonProperty("targetMemberNickname")
+    private Nickname targetMemberNickname;
+
+    @JsonProperty("mabcciNickname")
+    private Nickname mabcciNickname;
+
+    @JsonProperty("top")
+    private String top;
+
+    @JsonProperty("bottom")
+    private String bottom;
+
+    @JsonProperty("shoes")
+    private String shoes;
+
+    @JsonProperty("accessory")
+    private String accessory;
+
+    @JsonProperty("description")
+    private String description;
+
+    private ProposalDetailFindResponse() {
+    }
+
+    public final static ProposalDetailFindResponse ofProposal(final Proposal proposal) {
+        return new ProposalDetailFindResponse(proposal.targetMember().nickname(), proposal.mabcci().nickname(),
+                proposal.top(), proposal.bottom(), proposal.shoes(), proposal.accessory(), proposal.description());
+    }
+
+    public ProposalDetailFindResponse(final Nickname targetMemberNickname, final Nickname mabcciNickname,
+                                      final String top, final String bottom, final String shoes,
+                                      final String accessory, final String description) {
+        this.targetMemberNickname = targetMemberNickname;
+        this.mabcciNickname = mabcciNickname;
+        this.top = top;
+        this.bottom = bottom;
+        this.shoes = shoes;
+        this.accessory = accessory;
+        this.description = description;
+    }
+
+}
