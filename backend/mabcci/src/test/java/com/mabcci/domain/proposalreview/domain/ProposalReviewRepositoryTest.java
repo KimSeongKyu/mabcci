3184479a7514d94990a84dd9e0a2f04318ecc4d5
김성규 <kskyu610@gmail.com>
2021-08-18 00:26:23 +0900

    [S05P13C107-115] [BE-kskyu610] test: ProposalReviewRepository 구현체 존재 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/domain/ProposalReviewRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.mabcci.domain.proposalreview.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@EnableJpaAuditing
+@DataJpaTest
+class ProposalReviewRepositoryTest {
+
+    @Autowired private ProposalReviewRepository proposalReviewRepository;
+
+    @DisplayName("ProposalReviewRepository 구현체 존재 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(proposalReviewRepository).isNotNull(),
+                () -> assertThat(proposalReviewRepository).isExactlyInstanceOf(ProposalReviewRepository.class)
+        );
+    }
+}
