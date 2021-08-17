13cc8b460845d78e2cf0ba949967d61a356d6e0c
김성규 <kskyu610@gmail.com>
2021-08-17 11:05:20 +0900

    [S05P13C107-107] [BE-kskyu610] feat: ProposalRepository 인터페이스 작성

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/proposal/domain/ProposalRepository.java
@@ -0,0 +1,6 @@
+package com.mabcci.domain.proposal.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+}
