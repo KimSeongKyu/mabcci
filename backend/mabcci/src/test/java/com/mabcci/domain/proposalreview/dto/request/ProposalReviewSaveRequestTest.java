6fd6c370139f82d6eb5d790e764a42323e83f826
김성규 <kskyu610@gmail.com>
2021-08-18 00:42:44 +0900

    [S05P13C107-116] [BE-kskyu610] test: ProposalReviewSaveRequest 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/dto/request/ProposalReviewSaveRequestTest.java
@@ -0,0 +1,28 @@
+package com.mabcci.domain.proposalreview.dto.request;
+
+import com.mabcci.domain.proposalreview.domain.StarRating;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ProposalReviewSaveRequestTest {
+
+    private ProposalReviewSaveRequest proposalReviewSaveRequest;
+
+    @BeforeEach
+    void setUp() {
+        proposalReviewSaveRequest = new ProposalReviewSaveRequestTest(1L, StarRating.ZERO, "내용");
+    }
+
+    @DisplayName("ProposalReviewSaveRequest 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(proposalReviewSaveRequest).isNotNull(),
+                () -> assertThat(proposalReviewSaveRequest).isExactlyInstanceOf(ProposalReviewSaveRequest.class)
+        );
+    }
+}
