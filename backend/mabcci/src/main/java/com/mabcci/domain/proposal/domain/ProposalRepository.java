d14379d1c8672cb3986558ade9be66fc404f7b74
김성규 <kskyu610@gmail.com>
2021-08-17 17:05:37 +0900

    [S05P13C107-108] [BE-kskyu610] feat: ProposalRepository 내가 제안한 제안서 리스트 조회 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/proposal/domain/ProposalRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/proposal/domain/ProposalRepository.java
@@ -1,6 +1,17 @@
 package com.mabcci.domain.proposal.domain;
 
+import com.mabcci.global.common.Nickname;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+
+    @Query("SELECT p " +
+            "FROM Proposal p " +
+            "JOIN FETCH p.mabcci m " +
+            "WHERE m.nickname = :mabcciNickname")
+    List<Proposal> findAllByMabcciNickname(@Param("mabcciNickname") Nickname mabcciNickname);
 }
