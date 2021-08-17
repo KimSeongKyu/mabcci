63872434bc575b9741ba1f8e6ca133b6f0028cd1
김성규 <kskyu610@gmail.com>
2021-08-17 21:57:16 +0900

    [S05P13C107-111] [BE-kskyu610] test: ProposalRepository 제안서 삭제 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/proposal/domain/ProposalRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposal/domain/ProposalRepositoryTest.java
@@ -116,4 +116,16 @@ class ProposalRepositoryTest {
 
         assertThat(foundProposal.id()).isEqualTo(proposal.id());
     }
+
+    @DisplayName("ProposalRepository 제안서 삭제 테스트")
+    @Test
+    void delete_by_id_test() {
+        testEntityManager.persist(proposal);
+
+        assertThat(proposalRepository.existsById(proposal.id())).isTrue();
+
+        proposalRepository.deleteById(proposal.id());
+
+        assertThat(proposalRepository.existsById(proposal.id())).isFalse();
+    }
 }
