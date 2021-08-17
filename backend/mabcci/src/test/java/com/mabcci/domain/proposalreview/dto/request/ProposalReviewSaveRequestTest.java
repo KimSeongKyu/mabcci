fcf1e8aa35bac5e47d0f5ed44354869ab8146298
김성규 <kskyu610@gmail.com>
2021-08-18 00:54:23 +0900

    [S05P13C107-116] [BE-kskyu610] test: ProposalReviewSaveRequest 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/dto/request/ProposalReviewSaveRequestTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/dto/request/ProposalReviewSaveRequestTest.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -33,6 +39,23 @@ class ProposalReviewSaveRequestTest {
                 () -> assertThat(proposalReviewSaveRequest.id()).isEqualTo(1L),
                 () -> assertThat(proposalReviewSaveRequest.starRating()).isEqualTo(StarRating.ZERO),
                 () -> assertThat(proposalReviewSaveRequest.content()).isEqualTo("내용")
+        );
+    }
+
+    @DisplayName("ProposalReviewSaveRequest 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final ProposalReviewSaveRequest invalidRequest = new ProposalReviewSaveRequest(null, null, null);
+
+        final Set<ConstraintViolation<ProposalReviewSaveRequest>> invalidPropertiesOfValidRequest =
+                validator.validate(proposalReviewSaveRequest);
+        final Set<ConstraintViolation<ProposalReviewSaveRequest>> invalidPropertiesOfInvalidRequest =
+                validator.validate(invalidRequest);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidRequest.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidRequest.size()).isEqualTo(2)
         );
     }
 }
