95f007eed5f3a32275796b828d6788a5c6c04e4c
김성규 <kskyu610@gmail.com>
2021-08-18 00:07:25 +0900

    [S05P13C107-115] [BE-kskyu610] test: ProposalReview 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/domain/ProposalReviewTest.java
@@ -0,0 +1,76 @@
+package com.mabcci.domain.proposalreview.domain;
+
+import com.mabcci.domain.member.domain.Gender;
+import com.mabcci.domain.member.domain.Member;
+import com.mabcci.domain.member.domain.MemberRole;
+import com.mabcci.domain.proposal.domain.Proposal;
+import com.mabcci.global.common.Email;
+import com.mabcci.global.common.Nickname;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.mabcci.domain.member.domain.MemberTest.DESCRIPTION;
+import static com.mabcci.domain.member.domain.MemberTest.PICTURE;
+import static com.mabcci.global.common.EmailTest.EMAIL;
+import static com.mabcci.global.common.NicknameTest.NICKNAME;
+import static com.mabcci.global.common.PasswordTest.PASSWORD;
+import static com.mabcci.global.common.PhoneTest.PHONE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ProposalReviewTest {
+
+    private ProposalReview proposalReview;
+    private Proposal proposal;
+    private Member targetMember;
+    private Member mabcci;
+
+    @BeforeEach
+    void setUp() {
+        targetMember = Member.Builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
+                .phone(PHONE)
+                .gender(Gender.MAN)
+                .description(DESCRIPTION)
+                .picture(PICTURE)
+                .memberRole(MemberRole.USER)
+                .build();
+        mabcci = Member.Builder()
+                .email(Email.of("mabcci@example.com"))
+                .password(PASSWORD)
+                .nickname(Nickname.of("mabcci"))
+                .phone(PHONE)
+                .gender(Gender.MAN)
+                .description(DESCRIPTION)
+                .picture(PICTURE)
+                .memberRole(MemberRole.MABCCI)
+                .build();
+        proposal = Proposal.builder()
+                .targetMember(targetMember)
+                .mabcci(mabcci)
+                .top("topPictureUrl")
+                .bottom("bottomPictureUrl")
+                .shoes("shoesPictureUrl")
+                .accessory("accessoryPictureUrl")
+                .description("description")
+                .build();
+        proposalReview = ProposalReview.builder()
+                .proposal(proposal)
+                .starRating(StarRating.ZERO)
+                .content("내용")
+                .build();
+    }
+
+    @DisplayName("ProposalReview 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(proposalReview).isNotNull(),
+                () -> assertThat(proposalReview).isExactlyInstanceOf(ProposalReview.class)
+        );
+    }
+
+}
