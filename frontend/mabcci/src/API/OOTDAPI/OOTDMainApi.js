b3b5cafbb5d0ecd62d579cdf9221be24b02377a6
최창영 <linkin2love@naver.com>
2021-08-17 10:23:36 +0900

    [S05P13C107-28][FE-linkin2love]feat:검색 api 작성

--- frontend/mabcci/src/API/OOTDAPI/OOTDMainApi.js
+++ frontend/mabcci/src/API/OOTDAPI/OOTDMainApi.js
@@ -1,12 +1,22 @@
 import instance from '../index';
 import { OOTDSearchUrl, OOTDLikeUrl } from '../ApiUrl';
 
-const OOTDFeedApi = async (category, page, nickname) => {
-  console.log(`${page}로 요청`);
+export const OOTDFeedApi = async (category, page, nickname) => {
   const OOTDListUrl = `/api/ootds/${nickname}?filter=${category}&size=20&page=${page}`;
   const response = await instance.get(OOTDListUrl);
   console.log(response);
   return response;
 };
 
-export default OOTDFeedApi;
+export const SearchListApi = async (keyword, category) => {
+  if (category === 'hashtag') {
+    const searchListUrl = `/api/hashtags/search?hashtag=${keyword}`;
+    const response = await instance.get(searchListUrl);
+    return response;
+  }
+  if (category === 'member') {
+    const searchListUrl = `/api/members/search?nickname=${keyword}`;
+    const response = await instance.get(searchListUrl);
+    return response;
+  }
+};
