a6bbd878509fdf31da8a4b685c9aac13be77cdd0
김성규 <kskyu610@gmail.com>
2021-08-17 16:44:39 +0900

    [S05P13C107-108] [BE-kskyu610] test: ProposalFindResponses 인스턴스 프로퍼티 유효성 검증 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/proposal/dto/response/ProposalFindResponsesTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposal/dto/response/ProposalFindResponsesTest.java
@@ -1,11 +1,16 @@
 package com.mabcci.domain.proposal.dto.response;
 
+import com.mabcci.global.common.Nickname;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import static com.mabcci.domain.member.domain.MemberTest.PICTURE;
 import static com.mabcci.global.common.NicknameTest.NICKNAME;
@@ -38,4 +43,21 @@ class ProposalFindResponsesTest {
         assertThat(proposalFindResponses.proposals()).contains(proposalFindResponse);
     }
 
+
+    @DisplayName("ProposalFindResponses 인스턴스 프로퍼티 유효성 검증 테스트")
+    @Test
+    void validate_test() {
+        final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final ProposalFindResponses invalidResponse = new ProposalFindResponses(null);
+
+        final Set<ConstraintViolation<ProposalFindResponses>> invalidPropertiesOfValidResponse =
+                validator.validate(proposalFindResponses);
+        final Set<ConstraintViolation<ProposalFindResponses>> invalidPropertiesOfInvalidResponse =
+                validator.validate(invalidResponse);
+
+        assertAll(
+                () -> assertThat(invalidPropertiesOfValidResponse.size()).isEqualTo(0),
+                () -> assertThat(invalidPropertiesOfInvalidResponse.size()).isEqualTo(1)
+        );
+    }
 }
