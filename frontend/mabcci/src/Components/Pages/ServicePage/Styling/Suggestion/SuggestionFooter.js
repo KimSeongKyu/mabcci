2f7540286fed0f75c42518d199c0ca833a59fe8f
임성애 <tjddo2791@naver.com>
2021-08-18 14:15:21 +0900

    [S05P13C107-41] [FE-seongaeee] fix: 제안서 footer 데이터 수정

--- frontend/mabcci/src/Components/Pages/ServicePage/Styling/Suggestion/SuggestionFooter.js
+++ frontend/mabcci/src/Components/Pages/ServicePage/Styling/Suggestion/SuggestionFooter.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const SuggestionFooter = () => {
+  const suggestion = useSelector(state => state.SuggestionReducer);
+
   return (
     <footer className="suggesion-footer">
       <div className="suggesion-user">
         <h4>For</h4>
-        <h2>젠킨스</h2>
+        <h2>{suggestion.targetMemberNickname}</h2>
       </div>
     </footer>
   );
