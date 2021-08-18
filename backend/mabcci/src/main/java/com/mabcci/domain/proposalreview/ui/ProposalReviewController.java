d9d8a3a13d1ec04be2549d2168aead56cf318834
김성규 <kskyu610@gmail.com>
2021-08-18 11:35:39 +0900

    [S05P13C107-117] [BE-kskyu610] feat: ProposalReviewController 인스턴스 제안서 id에 해당하는 제안서 리뷰 상세 조회 API 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/proposalreview/ui/ProposalReviewController.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/proposalreview/ui/ProposalReviewController.java
@@ -1,28 +1,34 @@
 package com.mabcci.domain.proposalreview.ui;
 
+import com.mabcci.domain.proposalreview.application.ProposalReviewFindService;
 import com.mabcci.domain.proposalreview.application.ProposalReviewSaveService;
 import com.mabcci.domain.proposalreview.dto.request.ProposalReviewSaveRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 @CrossOrigin(originPatterns = "http://localhost:*")
 @RestController
 public class ProposalReviewController {
 
     private final ProposalReviewSaveService proposalReviewSaveService;
+    private final ProposalReviewFindService proposalReviewFindService;
 
-    public ProposalReviewController(final ProposalReviewSaveService proposalReviewSaveService) {
+    public ProposalReviewController(final ProposalReviewSaveService proposalReviewSaveService, final ProposalReviewFindService proposalReviewFindService) {
         this.proposalReviewSaveService = proposalReviewSaveService;
+        this.proposalReviewFindService = proposalReviewFindService;
     }
 
     @PostMapping("/api/proposals/reviews")
     public ResponseEntity saveProposalReview(@Valid @RequestBody final ProposalReviewSaveRequest proposalReviewSaveRequest) {
         proposalReviewSaveService.saveProposalReview(proposalReviewSaveRequest);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/api/proposals/{id}/reviews/details")
+    public ResponseEntity findProposalReview(@Positive @PathVariable("id") final Long id) {
+        return ResponseEntity.ok(proposalReviewFindService.findProposalReviewByProposalId(id));
     }
 }
