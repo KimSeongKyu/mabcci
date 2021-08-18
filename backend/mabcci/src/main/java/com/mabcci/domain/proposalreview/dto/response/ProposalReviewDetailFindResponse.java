da9361974f13b9f0f31596f72fd9351cb376bccf
김성규 <kskyu610@gmail.com>
2021-08-18 11:00:28 +0900

    [S05P13C107-117] [BE-kskyu610] feat: ProposalReviewDetailFindResponse 인스턴스 생성 기능 구현

---
+++ backend/mabcci/src/main/java/com/mabcci/domain/proposalreview/dto/response/ProposalReviewDetailFindResponse.java
@@ -0,0 +1,29 @@
+package com.mabcci.domain.proposalreview.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mabcci.domain.proposalreview.domain.ProposalReview;
+import com.mabcci.domain.proposalreview.domain.StarRating;
+
+import javax.validation.constraints.NotNull;
+
+public final class ProposalReviewDetailFindResponse {
+
+    @NotNull
+    @JsonProperty("starRating")
+    private StarRating starRating;
+
+    @JsonProperty("content")
+    private String content;
+
+    private ProposalReviewDetailFindResponse() {
+    }
+
+    public final static ProposalReviewDetailFindResponse ofProposalReview(final ProposalReview proposalReview) {
+        return new ProposalReviewDetailFindResponse(proposalReview.starRating(), proposalReview.content());
+    }
+
+    public ProposalReviewDetailFindResponse(final StarRating starRating, final String content) {
+        this.starRating = starRating;
+        this.content = content;
+    }
+}
