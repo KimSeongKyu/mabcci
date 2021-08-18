4d9f1760dba27d69750042ba276e6a69c1b6d8ce
김성규 <kskyu610@gmail.com>
2021-08-18 11:14:02 +0900

    [S05P13C107-117] [BE-kskyu610] test: ProposalReviewRepository 제안서 리뷰 저장 테스트 작성

--- backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/domain/ProposalReviewRepositoryTest.java
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/domain/ProposalReviewRepositoryTest.java
@@ -1,11 +1,24 @@
 package com.mabcci.domain.proposalreview.domain;
 
+import com.mabcci.domain.member.domain.Gender;
+import com.mabcci.domain.member.domain.Member;
+import com.mabcci.domain.member.domain.MemberRole;
+import com.mabcci.domain.proposal.domain.Proposal;
+import com.mabcci.global.common.Email;
+import com.mabcci.global.common.Nickname;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import static com.mabcci.domain.member.domain.MemberTest.DESCRIPTION;
+import static com.mabcci.domain.member.domain.MemberTest.PICTURE;
+import static com.mabcci.global.common.EmailTest.EMAIL;
+import static com.mabcci.global.common.NicknameTest.NICKNAME;
+import static com.mabcci.global.common.PasswordTest.PASSWORD;
+import static com.mabcci.global.common.PhoneTest.PHONE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -15,6 +28,49 @@ class ProposalReviewRepositoryTest {
 
     @Autowired private ProposalReviewRepository proposalReviewRepository;
 
+    private ProposalReview proposalReview;
+    private Proposal proposal;
+    private Member targetMember;
+    private Member mabcci;
+
+    @BeforeEach
+    void setUp() {
+        targetMember = Member.Builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
+                .phone(PHONE)
+                .gender(Gender.MAN)
+                .description(DESCRIPTION)
+                .picture(PICTURE)
+                .memberRole(MemberRole.USER)
+                .build();
+        mabcci = Member.Builder()
+                .email(Email.of("mabcci@example.com"))
+                .password(PASSWORD)
+                .nickname(Nickname.of("mabcci"))
+                .phone(PHONE)
+                .gender(Gender.MAN)
+                .description(DESCRIPTION)
+                .picture(PICTURE)
+                .memberRole(MemberRole.MABCCI)
+                .build();
+        proposal = Proposal.builder()
+                .targetMember(targetMember)
+                .mabcci(mabcci)
+                .top("topPictureUrl")
+                .bottom("bottomPictureUrl")
+                .shoes("shoesPictureUrl")
+                .accessory("accessoryPictureUrl")
+                .description("description")
+                .build();
+        proposalReview = ProposalReview.builder()
+                .proposal(proposal)
+                .starRating(StarRating.ZERO)
+                .content("내용")
+                .build();
+    }
+
     @DisplayName("ProposalReviewRepository 구현체 존재 여부 테스트")
     @Test
     void initialize() {
@@ -22,5 +78,13 @@ class ProposalReviewRepositoryTest {
                 () -> assertThat(proposalReviewRepository).isNotNull(),
                 () -> assertThat(proposalReviewRepository).isInstanceOf(ProposalReviewRepository.class)
         );
+    }
+
+    @DisplayName("ProposalReviewRepository 제안서 리뷰 저장 테스트")
+    @Test
+    void save_test() {
+        final ProposalReview savedReview = proposalReviewRepository.save(proposalReview);
+
+        assertThat(savedReview.id()).isEqualTo(proposalReview.id());
     }
 }
