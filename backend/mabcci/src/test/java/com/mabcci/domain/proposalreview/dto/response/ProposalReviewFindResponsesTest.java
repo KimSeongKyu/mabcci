6e346ec82cd82800818778f06c019f2803ec2cc4
김성규 <kskyu610@gmail.com>
2021-08-18 14:22:32 +0900

    [S05P13C107-117] [BE-kskyu610] test: ProposalReviewFindResponses 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/dto/response/ProposalReviewFindResponsesTest.java
@@ -0,0 +1,32 @@
+package com.mabcci.domain.proposalreview.dto.response;
+
+import com.mabcci.domain.proposalreview.domain.StarRating;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ProposalReviewFindResponsesTest {
+
+    private ProposalReviewFindResponses proposalReviewFindResponses;
+    private ProposalReviewFindResponse proposalReviewFindResponse;
+
+    @BeforeEach
+    void setUp() {
+        proposalReviewFindResponse = new ProposalReviewFindResponse(StarRating.ZERO.ordinal(), "내용");
+        proposalReviewFindResponses = new ProposalReviewFindResponses(List.of(proposalReviewFindResponse));
+    }
+
+    @DisplayName("ProposalReviewFindResponses 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(proposalReviewFindResponses).isNotNull(),
+                () -> assertThat(proposalReviewFindResponses).isExactlyInstanceOf(ProposalReviewFindResponses.class)
+        );
+    }
+}
