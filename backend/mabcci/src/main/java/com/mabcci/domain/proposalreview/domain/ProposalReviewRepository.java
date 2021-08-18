6cc4cdf7b346e2e68938e23c1c4085bd9893e2c7
김성규 <kskyu610@gmail.com>
2021-08-18 11:25:36 +0900

    [S05P13C107-117] [BE-kskyu610] feat: ProposalReviewRepository 제안서 id에 해당하는 제안서 리뷰 조회 기능 구현

--- backend/mabcci/src/main/java/com/mabcci/domain/proposalreview/domain/ProposalReviewRepository.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/proposalreview/domain/ProposalReviewRepository.java
@@ -1,6 +1,16 @@
 package com.mabcci.domain.proposalreview.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProposalReviewRepository extends JpaRepository<ProposalReview, Long> {
+
+    @Query("SELECT p_r " +
+            "FROM ProposalReview p_r " +
+            "JOIN FETCH p_r.proposal p " +
+            "WHERE p.id = :proposalId")
+    Optional<ProposalReview> findByProposalId(@Param("proposalId") Long proposalId);
 }
