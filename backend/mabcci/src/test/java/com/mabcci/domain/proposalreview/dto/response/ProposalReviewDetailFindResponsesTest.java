6e9e823af0ec3e1e761eb9d96b9e171a224d8e4d
김성규 <kskyu610@gmail.com>
2021-08-18 18:38:12 +0900

    [S05P13C107-117] [BE-kskyu610] test: ProposalReviewDetailFindResponses 인스턴스 생성 여부 테스트 작성

---
+++ backend/mabcci/src/test/java/com/mabcci/domain/proposalreview/dto/response/ProposalReviewDetailFindResponsesTest.java
@@ -0,0 +1,35 @@
+package com.mabcci.domain.proposalreview.dto.response;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.mabcci.domain.member.domain.MemberTest.PICTURE;
+import static com.mabcci.global.common.NicknameTest.NICKNAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ProposalReviewDetailFindResponsesTest {
+
+    private ProposalReviewDetailFindResponse proposalReviewDetailFindResponse;
+    private ProposalReviewDetailFindResponses proposalReviewDetailFindResponses;
+
+    @BeforeEach
+    void setUp() {
+        proposalReviewDetailFindResponse =
+                new ProposalReviewDetailFindResponse(PICTURE, NICKNAME, 0, "내용", LocalDateTime.now());
+        proposalReviewDetailFindResponses = new ProposalReviewDetailFindResponses(List.of(proposalReviewDetailFindResponse));
+    }
+
+    @DisplayName("ProposalReviewDetailFindResponses 인스턴스 생성 여부 테스트")
+    @Test
+    void initialize() {
+        assertAll(
+                () -> assertThat(proposalReviewDetailFindResponses).isNotNull(),
+                () -> assertThat(proposalReviewDetailFindResponses).isExactlyInstanceOf(ProposalReviewDetailFindResponses.class)
+        );
+    }
+}
