626e9beb00d7b397def55db54da154872b6ae638
김우재 <kwj1270@naver.com>
2021-08-19 13:54:13 +0900

    [S05P13C107-102] [BE-kwj1270] fix: 로그인 통일성 맞추도록 리팩토링

--- backend/mabcci/src/main/java/com/mabcci/domain/member/application/MemberJoinService.java
+++ backend/mabcci/src/main/java/com/mabcci/domain/member/application/MemberJoinService.java
@@ -1,5 +1,11 @@
 package com.mabcci.domain.member.application;
 
+import com.mabcci.domain.auth.common.JwtUtil;
+import com.mabcci.domain.auth.domain.RefreshToken;
+import com.mabcci.domain.auth.domain.RefreshTokenRepository;
+import com.mabcci.domain.auth.domain.vo.JwtToken;
+import com.mabcci.domain.auth.domain.vo.JwtTokenType;
+import com.mabcci.domain.auth.dto.response.LoginResponse;
 import com.mabcci.domain.category.domain.Category;
 import com.mabcci.domain.category.domain.CategoryRepository;
 import com.mabcci.domain.member.domain.Member;
@@ -7,6 +13,7 @@ import com.mabcci.domain.member.domain.MemberRepository;
 import com.mabcci.domain.member.domain.MemberSpecs;
 import com.mabcci.domain.membercategory.domain.MemberCategory;
 import com.mabcci.domain.picture.common.PictureUtil;
+import com.mabcci.global.common.Email;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +24,21 @@ public class MemberJoinService {
 
     private final MemberRepository memberRepository;
     private final CategoryRepository categoryRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtUtil jwtUtil;
 
-    public MemberJoinService(final MemberRepository memberRepository, final CategoryRepository categoryRepository) {
+    public MemberJoinService(final MemberRepository memberRepository,
+                             final CategoryRepository categoryRepository,
+                             final RefreshTokenRepository refreshTokenRepository,
+                             final JwtUtil jwtUtil) {
         this.memberRepository = memberRepository;
         this.categoryRepository = categoryRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     @Transactional
-    public Member join(final Member member, final Set<String> categoryNames) {
+    public LoginResponse join(final Member member, final Set<String> categoryNames) {
         final MemberSpecs memberSpecs = MemberSpecs.noContent();
         member.updateMemberSpecs(memberSpecs);
         for (String categoryName : categoryNames) {
@@ -32,12 +46,30 @@ public class MemberJoinService {
             final MemberCategory memberCategory = MemberCategory.fromMemberAndCategory(member, category);
             member.addMemberCategory(memberCategory);
         }
-        return memberRepository.save(member);
+        final Member savedMember = memberRepository.save(member);
+        final JwtToken accessToken = jwtUtil.createToken(JwtTokenType.ACCESS_TOKEN, member);
+        final JwtToken refreshToken = jwtUtil.createToken(JwtTokenType.REFRESH_TOKEN, member);
+
+        saveRefreshToken(savedMember.email(), refreshToken);
+        return new LoginResponse(accessToken, refreshToken);
     }
+
 
     private Category getCategoryByCategoryName(final String categoryName) {
         return categoryRepository.findByCategoryName(categoryName)
                 .orElseThrow(IllegalArgumentException::new);
     }
+
+    private void saveRefreshToken(final Email email, final JwtToken refreshToken) {
+        refreshTokenRepository.save(buildRefreshToken(email, refreshToken));
+    }
+
+    private RefreshToken buildRefreshToken(final Email email, final JwtToken refreshToken) {
+        return RefreshToken.builder()
+                .email(email)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
 
 }
